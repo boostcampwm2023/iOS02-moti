44f766d5f5da822060fd079eb985e4a564423a58
유정주 <dbwjdwn9216@gmail.com>
2023-11-08 15:37:16 +0900

    [iOS] feat: moti 프로젝트 생성

---
+++ iOS/moti/moti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moti
+//
+//  Created by 유정주 on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
