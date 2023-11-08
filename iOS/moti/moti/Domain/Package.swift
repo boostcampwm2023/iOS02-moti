78a86f05ba1771162441a08ef5b858d05e168e6d
유정주 <dbwjdwn9216@gmail.com>
2023-11-08 22:05:37 +0900

    [iOS] feat: Domain 레이어 패키지 생성

---
+++ iOS/moti/moti/Domain/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Domain",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Domain",
+            targets: ["Domain"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Domain"),
+        .testTarget(
+            name: "DomainTests",
+            dependencies: ["Domain"]),
+    ]
+)
