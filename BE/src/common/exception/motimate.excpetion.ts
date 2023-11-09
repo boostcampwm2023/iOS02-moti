d4472473c7ca2900d7159ea52f8564af5b732915
Dltmd202 <dltmd202@gmail.com>
2023-11-10 00:08:27 +0900

    [BE] fix: motimateException를 HttpException의 서브타입화

--- BE/src/common/exception/motimate.excpetion.ts
+++ BE/src/common/exception/motimate.excpetion.ts
@@ -1,12 +1,12 @@
+import { HttpException } from "@nestjs/common";
+
 export interface ErrorCode {
   statusCode: number;
   message: string;
 }
 
-export class MotimateException extends Error {
-  public readonly statusCode: number;
+export class MotimateException extends HttpException{
   constructor(errorCode: ErrorCode) {
-    super(errorCode.message);
-    this.statusCode = errorCode.statusCode;
+    super(errorCode.message, errorCode.statusCode);
   }
 }
--- BE/src/common/filter/exception.filter.ts
+++ BE/src/common/filter/exception.filter.ts
@@ -1,16 +1,15 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { MotimateException } from '../exception/motimate.excpetion';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
 import { Request, Response } from 'express';
 
-@Catch(MotimateException)
+@Catch(HttpException)
 export class MotimateExceptionFilter
-  implements ExceptionFilter<MotimateException>
+  implements ExceptionFilter<HttpException>
 {
-  catch(exception: MotimateException, host: ArgumentsHost): any {
+  catch(exception: HttpException, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.statusCode;
+    const status = exception.getStatus();
 
     response.status(status).json({
       statusCode: status,
