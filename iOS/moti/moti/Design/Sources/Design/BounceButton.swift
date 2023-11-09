7331a8862f4daaf4d72f7820f3cd05b3bbfb6277
유정주 <dbwjdwn9216@gmail.com>
2023-11-09 18:06:53 +0900

    [iOS] feat: BounceButton 애니메이션 구현

--- iOS/moti/moti/Design/Sources/Design/BounceButton.swift
+++ iOS/moti/moti/Design/Sources/Design/BounceButton.swift
@@ -10,10 +10,12 @@ import UIKit
 open class BounceButton: UIButton {
     open override var isHighlighted: Bool {
         didSet {
-            isHighlighted ? highlight() : nonhighlight()
+            isHighlighted ? hightlightAnimation() : nonhighlightAnimation()
         }
     }
-
+    
+    private let animationDuration = 0.08
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -26,21 +28,41 @@ open class BounceButton: UIButton {
     
     private func setupUI() {
         layer.cornerRadius = CornerRadius.big
+        layer.cornerCurve = .continuous
         layer.borderWidth = 1
-                
+        
         setTitleColor(.buttonTitleColor, for: .normal)
         setTitleColor(.bounceButtonHighlightTitleColor, for: .highlighted)
         
-        nonhighlight()
+        updateButtonColor()
     }
     
-    private func highlight() {
-        backgroundColor = .primaryBlue
-        layer.borderColor = UIColor.primaryBlue.cgColor
+    private func updateButtonColor() {
+        if isHighlighted {
+            backgroundColor = .primaryBlue
+            layer.borderColor = UIColor.primaryBlue.cgColor
+        } else {
+            backgroundColor = .primaryGray
+            layer.borderColor = UIColor.primaryDarkGray.cgColor
+        }
     }
     
-    private func nonhighlight() {
-        backgroundColor = .primaryGray
-        layer.borderColor = UIColor.primaryDarkGray.cgColor
+    private func hightlightAnimation() {
+        UIView.animate(withDuration: animationDuration) {
+            self.updateButtonColor()
+            
+            // 95%로 크기를 줄임
+            let scale = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            self.transform = scale
+        }
+    }
+    
+    private func nonhighlightAnimation() {
+        UIView.animate(withDuration: animationDuration) {
+            self.updateButtonColor()
+            
+            // 원래의 크기로 되돌림
+            self.transform = .identity
+        }
     }
 }
