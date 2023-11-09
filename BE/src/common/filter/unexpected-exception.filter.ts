18b84c457ae197f0952df0a5a87bb4972d87dac8
Dltmd202 <dltmd202@gmail.com>
2023-11-10 00:06:57 +0900

    [BE] fix: UnexpectedExceptionFilter 적용 범위 수정
    - Error가 아닌 모든 Instance catch

--- BE/src/common/filter/unexpected-exception.filter.ts
+++ BE/src/common/filter/unexpected-exception.filter.ts
@@ -1,9 +1,9 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-@Catch(Error)
-export class UnexpectedExceptionFilter implements ExceptionFilter<Error> {
-  catch(exception: Error, host: ArgumentsHost): any {
+@Catch()
+export class UnexpectedExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
