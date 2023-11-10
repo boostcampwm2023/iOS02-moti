235e5be29f792397e3157678c0bcea053f620e19
Dltmd202 <dltmd202@gmail.com>
2023-11-11 03:06:27 +0900

    [BE] chore: 테스트 엔티티를 위한 typeorm Option 수정

--- BE/src/config/typeorm/index.ts
+++ BE/src/config/typeorm/index.ts
@@ -12,7 +12,7 @@ export const typeOrmModuleOptions = {
       username: configService.get('DB_USERNAME'),
       password: configService.get('DB_PASSWORD'),
       database: configService.get('DB_DATABASE'),
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      entities: [configService.get('DB_ENTITIES')],
       logging: true,
       synchronize: true,
       namingStrategy: new CamelSnakeNameStrategy(),
