e56cce4e67873a40f934f41b79bd657826814618
lsh23 <shseoul14@gmail.com>
2023-11-12 00:04:41 +0900

    [BE] feat: apple ID 서버로 부터 public key를 가져와서 identityToken을 검증한다.

---
+++ BE/src/auth/application/oauth-handler.ts
@@ -0,0 +1,26 @@
+import { OauthRequester } from './oauth-requester';
+import { Injectable } from '@nestjs/common';
+import { IdentityToken } from './identity-token';
+import { JwtUtils } from './jwt-utils';
+import { PublicKey } from '../index';
+
+@Injectable()
+export class OauthHandler {
+  constructor(
+    private readonly oauthRequester: OauthRequester,
+    private readonly jwtUtils: JwtUtils,
+  ) {}
+  async getUserIdentifier(token: string): Promise<string> {
+    const identityToken = new IdentityToken(token);
+    const publicKeys = await this.oauthRequester.getPublicKeys();
+    const publicKey = publicKeys.keys.find(
+      (key) => key.kid === identityToken.header.kid,
+    );
+    this.validateIdentityToken(identityToken.jwt, publicKey);
+    const payloads = this.jwtUtils.parsePayloads(identityToken.jwt);
+    return payloads['sub'];
+  }
+  private validateIdentityToken(identityToken: string, publicKey: PublicKey) {
+    this.jwtUtils.validate(identityToken, publicKey);
+  }
+}
