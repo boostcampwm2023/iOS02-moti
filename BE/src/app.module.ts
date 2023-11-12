1a2b16960ce51eb505b190cb0dad8e616a8dea83
Dltmd202 <dltmd202@gmail.com>
2023-11-12 20:25:36 +0900

    [BE] feat: Operate 모듈 등록

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { typeOrmModuleOptions } from './config/typeorm';
 import { configServiceModuleOptions } from './config/config';
+import { OperateModule } from './operate/operate.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configServiceModuleOptions),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    OperateModule,
   ],
   controllers: [AppController],
   providers: [AppService],
