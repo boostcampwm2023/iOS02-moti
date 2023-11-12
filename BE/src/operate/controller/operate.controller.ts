162c69aa373feb1ed10bf1153d7c1b883c0e0e6c
Dltmd202 <dltmd202@gmail.com>
2023-11-12 20:25:14 +0900

    [BE] feat: Operate 컨트롤러 생성
    - 모티메이트 운영정책 초기화 API
    - 모티메이트 운영정책 조회 API

---
+++ BE/src/operate/controller/operate.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { OperateService } from '../application/operate.service';
+import { MotiPolicyResponse } from '../dto/moti-policy-response';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { MotiPolicyCreate } from '../dto/moti-policy-create';
+
+@Controller('operate')
+@ApiTags('운영 API')
+export class OperateController {
+  constructor(private readonly operateService: OperateService) {}
+
+  @Post('policy')
+  @ApiOperation({
+    summary: '모티메이트 운영정책 초기화 API',
+    description: '운영 정책은 1개만 등록 가능',
+  })
+  async initPolicy(@Body() initPolicy: MotiPolicyCreate) {
+    const policy = await this.operateService.initMotiPolicy(initPolicy);
+    return MotiPolicyResponse.from(policy);
+  }
+
+  @Get('policy')
+  @ApiOperation({
+    summary: '모티메이트 운영정책 조회 API',
+    description: '모티메이트의 현재 버전, 최소 번전, 보안 정책을 조회',
+  })
+  async getPolicy() {
+    const policy = await this.operateService.retrieveMotimateOperation();
+    return MotiPolicyResponse.from(policy);
+  }
+}
