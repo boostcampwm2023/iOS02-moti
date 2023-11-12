b95ba2ab050dc63df1894cfdf1d190bbc5514029
Dltmd202 <dltmd202@gmail.com>
2023-11-12 20:14:27 +0900

    [BE] feat: Operate 서비스 생성
    - OperateService는 MotiPolicy만을 위한 서비스가 아닌 운영을 위한 서비스를 모아놓는 것으로 계획

--- BE/src/common/exception/motimate.excpetion.ts
+++ BE/src/common/exception/motimate.excpetion.ts
@@ -5,6 +5,16 @@ export interface ErrorCode {
   message: string | object;
 }
 
+export const VERSION_NOT_FOUND = {
+  statusCode: 500,
+  message: '운영정책을 조회할 수 없습니다.',
+};
+
+export const VERSION_ALREADY_EXISTS = {
+  statusCode: 500,
+  message: '이미 초기화된 모티메이트 운영정책입니다.',
+};
+
 export class MotimateException extends HttpException {
   constructor(errorCode: ErrorCode) {
     super(errorCode.message, errorCode.statusCode);
