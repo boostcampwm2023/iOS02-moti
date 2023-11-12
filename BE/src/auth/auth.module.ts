12c4c36b380cf86e72875fcabf13d755f8810fc1
lsh23 <shseoul14@gmail.com>
2023-11-12 17:20:47 +0900

    [BE] fix: 미구현 모듈을 제외한다.

--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './application/auth.service';
-import { AuthController } from './controller/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [],
+  providers: [],
 })
 export class AuthModule {}
