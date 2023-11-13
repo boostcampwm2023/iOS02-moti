04a60f1e019ee511ea399f53cc7019475bb042f8
Dltmd202 <dltmd202@gmail.com>
2023-11-13 22:18:39 +0900

    [BE] fix: 레포지토리 테스트 트랜잭션 적용

--- BE/src/operate/entities/moti-policy-repository.spec.ts
+++ BE/src/operate/entities/moti-policy-repository.spec.ts
@@ -7,6 +7,7 @@ import { OperateModule } from '../operate.module';
 import { configServiceModuleOptions } from '../../config/config';
 import { MotiPolicy } from '../domain/moti-policy.domain';
 import { DataSource } from 'typeorm';
+import { transactionTest } from '../../../test/common/transaction-test';
 
 describe('MotiPolicyRepository Test', () => {
   let motiPolicyRepository: MotiPolicyRepository;
@@ -31,10 +32,6 @@ describe('MotiPolicyRepository Test', () => {
     await motiPolicyRepository.delete({});
   });
 
-  afterAll(async () => {
-    await dataSource.destroy();
-  });
-
   describe('테스트 환경 확인', () => {
     it('motiPolicyRepository가 정의되어 있어야 한다.', () => {
       expect(motiPolicyRepository).toBeDefined();
@@ -42,49 +39,55 @@ describe('MotiPolicyRepository Test', () => {
   });
 
   test('savePolicy는 모티메이트 운영정책을 초기화한다.', async () => {
-    // given
-    const initialPolicy = new MotiPolicy(
-      '0.2.0',
-      '0.1.0',
-      'https://motimate.com/policy',
-    );
-    await motiPolicyRepository.savePolicy(initialPolicy);
+    await transactionTest(dataSource, async () => {
+      // given
+      const initialPolicy = new MotiPolicy(
+        '0.2.0',
+        '0.1.0',
+        'https://motimate.com/policy',
+      );
+      await motiPolicyRepository.savePolicy(initialPolicy);
 
-    // when
-    const motiPolicy = await motiPolicyRepository.findLatestPolicy();
+      // when
+      const motiPolicy = await motiPolicyRepository.findLatestPolicy();
 
-    // then
-    expect(motiPolicy).toBeDefined();
-    expect(motiPolicy.latest).toBe('0.2.0');
-    expect(motiPolicy.required).toBe('0.1.0');
-    expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+      // then
+      expect(motiPolicy).toBeDefined();
+      expect(motiPolicy.latest).toBe('0.2.0');
+      expect(motiPolicy.required).toBe('0.1.0');
+      expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+    });
   });
 
   test('findLatestPolicy는 가장 최근의 애플리케이션 규약을 조회한다.', async () => {
-    // given
-    const initialPolicy = new MotiPolicy(
-      '0.2.0',
-      '0.1.0',
-      'https://motimate.com/policy',
-    );
-    await motiPolicyRepository.savePolicy(initialPolicy);
+    await transactionTest(dataSource, async () => {
+      // given
+      const initialPolicy = new MotiPolicy(
+        '0.2.0',
+        '0.1.0',
+        'https://motimate.com/policy',
+      );
+      await motiPolicyRepository.savePolicy(initialPolicy);
 
-    // when
-    const motiPolicy = await motiPolicyRepository.findLatestPolicy();
+      // when
+      const motiPolicy = await motiPolicyRepository.findLatestPolicy();
 
-    // then
-    expect(motiPolicy).toBeDefined();
-    expect(motiPolicy.latest).toBe('0.2.0');
-    expect(motiPolicy.required).toBe('0.1.0');
-    expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+      // then
+      expect(motiPolicy).toBeDefined();
+      expect(motiPolicy.latest).toBe('0.2.0');
+      expect(motiPolicy.required).toBe('0.1.0');
+      expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+    });
   });
 
   test('fincLatestPolicy는 초기화되지 않은 상태에서 undefined을 반환한다.', async () => {
-    // given
-    // when
-    const motiPolicy = await motiPolicyRepository.findLatestPolicy();
+    await transactionTest(dataSource, async () => {
+      // given
+      // when
+      const motiPolicy = await motiPolicyRepository.findLatestPolicy();
 
-    // then
-    expect(motiPolicy).toBeUndefined();
+      // then
+      expect(motiPolicy).toBeUndefined();
+    });
   });
 });
