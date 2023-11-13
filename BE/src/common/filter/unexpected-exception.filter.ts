36f2d7b0377278bfbd4749a443d9bede72f0b372
Dltmd202 <dltmd202@gmail.com>
2023-11-14 01:31:57 +0900

    [BE] feat: 기존 API 응답 코드에 ApiData 적용

--- BE/src/common/filter/unexpected-exception.filter.ts
+++ BE/src/common/filter/unexpected-exception.filter.ts
@@ -1,18 +1,13 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { ApiData } from '../api/api-data';
 
 @Catch()
 export class UnexpectedExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
 
-    response.status(500).json({
-      statusCode: 500,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      message: 'Internal Server Error',
-    });
+    response.status(500).json(ApiData.error('Internal Server Error'));
   }
 }
--- BE/src/operate/controller/operate.controller.ts
+++ BE/src/operate/controller/operate.controller.ts
@@ -3,6 +3,7 @@ import { OperateService } from '../application/operate.service';
 import { MotiPolicyResponse } from '../dto/moti-policy-response';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { MotiPolicyCreate } from '../dto/moti-policy-create';
+import { ApiData } from '../../common/api/api-data';
 
 @Controller('operate')
 @ApiTags('운영 API')
@@ -14,9 +15,11 @@ export class OperateController {
     summary: '모티메이트 운영정책 초기화 API',
     description: '운영 정책은 1개만 등록 가능',
   })
-  async initPolicy(@Body() initPolicy: MotiPolicyCreate) {
+  async initPolicy(
+    @Body() initPolicy: MotiPolicyCreate,
+  ): Promise<ApiData<MotiPolicyResponse>> {
     const policy = await this.operateService.initMotiPolicy(initPolicy);
-    return MotiPolicyResponse.from(policy);
+    return ApiData.success(MotiPolicyResponse.from(policy));
   }
 
   @Get('policy')
@@ -24,8 +27,8 @@ export class OperateController {
     summary: '모티메이트 운영정책 조회 API',
     description: '모티메이트의 현재 버전, 최소 번전, 보안 정책을 조회',
   })
-  async getPolicy() {
+  async getPolicy(): Promise<ApiData<MotiPolicyResponse>> {
     const policy = await this.operateService.retrieveMotimateOperation();
-    return MotiPolicyResponse.from(policy);
+    return ApiData.success(MotiPolicyResponse.from(policy));
   }
 }
