b26177f547eb0fb673613f1d9fe1c0ee14ef1d0e
lsh23 <shseoul14@gmail.com>
2023-11-13 23:16:57 +0900

    [BE] feat: user repository를 구현한다.

---
+++ BE/src/users/entities/user.repository.spec.ts
@@ -0,0 +1,57 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserRepository } from './user.repository';
+import { CustomTypeOrmModule } from '../../config/typeorm/custom-typeorm.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from '../../config/typeorm';
+import { User } from '../domain/user.domain';
+import { configServiceModuleOptions } from '../../config/config';
+import { UserEntity } from './user.entity';
+
+describe('UserRepository test', () => {
+  let usersRepository: UserRepository;
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(configServiceModuleOptions),
+        TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+        CustomTypeOrmModule.forCustomRepository([UserRepository]),
+      ],
+    }).compile();
+
+    usersRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  beforeEach(() => {
+    usersRepository.delete({});
+  });
+
+  test('userIdentifier로 user를 조회할 수 있다.', async () => {
+    // given
+    const user = User.from('userIdentifier');
+    user.assignUserCode('A1B2C1D');
+    await usersRepository.save(UserEntity.from(user));
+
+    // when
+    const findOne =
+      await usersRepository.findOneByUserIdentifier('userIdentifier');
+
+    // then
+    expect(findOne.userIdentifier).toBe('userIdentifier');
+  });
+
+  test('userCode로 user 존재 유뮤를 알 수 있다.', async () => {
+    // given
+    const user = User.from('userIdentifier');
+    user.assignUserCode('A1B2C1D');
+    await usersRepository.save(UserEntity.from(user));
+
+    // when
+    const existByUserCode = await usersRepository.existByUserCode('A1B2C1D');
+    const nonExistByUserCode = await usersRepository.existByUserCode('A1B2C1E');
+
+    // then
+    expect(existByUserCode).toBe(true);
+    expect(nonExistByUserCode).toBe(false);
+  });
+});
