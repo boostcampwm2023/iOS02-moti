232d89699593800f80da49b80e72e616630c9531
Dltmd202 <dltmd202@gmail.com>
2023-11-13 21:35:40 +0900

    [BE] fix: operate 테스트 코드 수정
    - 트랜잭션 매니저가 동작하지 않기 때문에 수동으로
    asyncQueryRunnerStorage를 띄워서 테스트

--- BE/src/operate/application/operate.service.spec.ts
+++ BE/src/operate/application/operate.service.spec.ts
@@ -1,18 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { OperateService } from './operate.service';
+import { MotiPolicyCreate } from '../dto/moti-policy-create';
+import { MotimateException } from '../../common/exception/motimate.excpetion';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmModuleOptions } from '../../config/typeorm';
 import { OperateModule } from '../operate.module';
 import { ConfigModule } from '@nestjs/config';
 import { configServiceModuleOptions } from '../../config/config';
-import { OperateService } from './operate.service';
-import { MotiPolicyCreate } from '../dto/moti-policy-create';
-import { DataSource, QueryRunner } from 'typeorm';
-import { MotimateException } from '../../common/exception/motimate.excpetion';
+import { DataSource } from 'typeorm';
+import { transactionTest } from '../../../test/common/transaction-test';
 
 describe('OperateService Test', () => {
   let operateService: OperateService;
   let dataSource: DataSource;
-  let queryRunner: QueryRunner;
 
   beforeAll(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -25,19 +25,9 @@ describe('OperateService Test', () => {
 
     operateService = app.get<OperateService>(OperateService);
     dataSource = app.get<DataSource>(DataSource);
-    queryRunner = dataSource.createQueryRunner();
-  });
-
-  beforeEach(async () => {
-    await queryRunner.startTransaction();
-  });
-
-  afterEach(async () => {
-    await queryRunner.rollbackTransaction();
   });
 
   afterAll(async () => {
-    await queryRunner.release();
     await dataSource.destroy();
   });
 
@@ -48,71 +38,79 @@ describe('OperateService Test', () => {
   });
 
   it('initMotiPolicy는 모티메이트 운영정책을 초기화한다.', async () => {
-    // given
-    const initialPolicy = new MotiPolicyCreate(
-      '0.2.0',
-      '0.1.0',
-      'https://motimate.com/policy',
-    );
+    await transactionTest(dataSource, async () => {
+      // given
+      const initialPolicy = new MotiPolicyCreate(
+        '0.2.0',
+        '0.1.0',
+        'https://motimate.com/policy',
+      );
 
-    // when
-    const motiPolicy = await operateService.initMotiPolicy(initialPolicy);
+      // when
+      const motiPolicy = await operateService.initMotiPolicy(initialPolicy);
 
-    // then
-    expect(motiPolicy).toBeDefined();
-    expect(motiPolicy.latest).toBe('0.2.0');
-    expect(motiPolicy.required).toBe('0.1.0');
-    expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+      // then
+      expect(motiPolicy).toBeDefined();
+      expect(motiPolicy.latest).toBe('0.2.0');
+      expect(motiPolicy.required).toBe('0.1.0');
+      expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+    });
   });
 
   it('initMotiPolicy는 모티메이트 운영정책을 초기화할 때, 이미 초기화된 상태에서 MotimateException을 발생시킨다.', async () => {
-    // given
-    const initialPolicy = new MotiPolicyCreate(
-      '0.2.0',
-      '0.1.0',
-      'https://motimate.com/policy',
-    );
-    await operateService.initMotiPolicy(initialPolicy);
-
-    // when
-    // then
-    try {
+    await transactionTest(dataSource, async () => {
+      // given
+      const initialPolicy = new MotiPolicyCreate(
+        '0.2.0',
+        '0.1.0',
+        'https://motimate.com/policy',
+      );
       await operateService.initMotiPolicy(initialPolicy);
-    } catch (e) {
-      expect(e).toBeInstanceOf(MotimateException);
-      expect(e.status).toBe(500);
-      expect(e.message).toBe('이미 초기화된 모티메이트 운영정책입니다.');
-    }
+
+      // when
+      // then
+      try {
+        await operateService.initMotiPolicy(initialPolicy);
+      } catch (e) {
+        expect(e).toBeInstanceOf(MotimateException);
+        expect(e.status).toBe(500);
+        expect(e.message).toBe('이미 초기화된 모티메이트 운영정책입니다.');
+      }
+    });
   });
 
   it('retrieveMotimateOperation는 모티메이트 운영정책이 없을 때 MotimateException을 발생시킨다.', async () => {
-    // given
-    // when
-    // then
-    try {
-      await operateService.retrieveMotimateOperation();
-    } catch (e) {
-      expect(e).toBeInstanceOf(MotimateException);
-      expect(e.status).toBe(500);
-      expect(e.message).toBe('운영정책을 조회할 수 없습니다.');
-    }
+    await transactionTest(dataSource, async () => {
+      // given
+      // when
+      // then
+      try {
+        await operateService.retrieveMotimateOperation();
+      } catch (e) {
+        expect(e).toBeInstanceOf(MotimateException);
+        expect(e.status).toBe(500);
+        expect(e.message).toBe('운영정책을 조회할 수 없습니다.');
+      }
+    });
   });
 
   it('retrieveMotimateOperation는 모티메이트 운영정책을 조회한다.', async () => {
-    // given
-    const initialPolicy = new MotiPolicyCreate(
-      '0.2.0',
-      '0.1.0',
-      'https://motimate.com/policy',
-    );
-    await operateService.initMotiPolicy(initialPolicy);
+    await transactionTest(dataSource, async () => {
+      // given
+      const initialPolicy = new MotiPolicyCreate(
+        '0.2.0',
+        '0.1.0',
+        'https://motimate.com/policy',
+      );
+      await operateService.initMotiPolicy(initialPolicy);
 
-    // when
-    const motiPolicy = await operateService.retrieveMotimateOperation();
-    // then
-    expect(motiPolicy).toBeDefined();
-    expect(motiPolicy.latest).toBe('0.2.0');
-    expect(motiPolicy.required).toBe('0.1.0');
-    expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+      // when
+      const motiPolicy = await operateService.retrieveMotimateOperation();
+      // then
+      expect(motiPolicy).toBeDefined();
+      expect(motiPolicy.latest).toBe('0.2.0');
+      expect(motiPolicy.required).toBe('0.1.0');
+      expect(motiPolicy.privacyPolicy).toBe('https://motimate.com/policy');
+    });
   });
 });
