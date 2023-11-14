52097adc165e19a1c854c73a4ca4d704dc4663a8
looloolalaa <dlrlgus65@nate.com>
2023-11-14 17:57:58 +0900

    [iOS] feat: HomeView & HomeVC 구현

--- iOS/moti/moti/Presentation/Sources/Presentation/Home/HomeView.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/Home/HomeView.swift
@@ -10,16 +10,18 @@ import UIKit
 final class HomeView: UIView {
     
     // 카테고리 리스트 컬렉션 뷰
-    private let categoryCollectionView = {
+    let categoryCollectionView = {
         let collectionView = UIView()
         collectionView.backgroundColor = .blue
         return collectionView
     }()
     
     // 달성 기록 리스트 컬렉션 뷰
-    private let recordCollectionView = {
-        let collectionView = UIView()
-        collectionView.backgroundColor = .red
+    lazy var recordCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCompositionalLayout())
+        collectionView.backgroundColor = .motiBackground
+        collectionView.register(RecordCollectionViewCell.self, forCellWithReuseIdentifier: RecordCollectionViewCell.identifier)
+        collectionView.register(with: HeaderView.self, elementKind: UICollectionView.elementKindSectionHeader)
         return collectionView
     }()
     
@@ -52,5 +54,42 @@ final class HomeView: UIView {
             .width(equalTo: self.widthAnchor)
             .top(equalTo: categoryCollectionView.bottomAnchor, constant: 10)
             .bottom(equalTo: self.bottomAnchor, constant: -10)
+    }
+}
+
+private extension HomeView {
+    func makeCompositionalLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let itemPadding: CGFloat = 1
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0 / 3),
+                heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: itemPadding, leading: itemPadding, bottom: itemPadding, trailing: itemPadding)
+
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(RecordCollectionViewCell.cellHeight))
+            
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item, item])
+//            iOS 16
+//            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, repeatingSubitem: item, count: 1)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            let headerSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .estimated(96))
+            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: headerSize,
+                elementKind: UICollectionView.elementKindSectionHeader,
+                alignment: .top)
+
+            section.boundarySupplementaryItems = [sectionHeader]
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+            return section
+        }
+        
+        return layout
     }
 }
--- iOS/moti/moti/Presentation/Sources/Presentation/Home/HomeViewController.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/Home/HomeViewController.swift
@@ -6,13 +6,89 @@
 //
 
 import UIKit
+import Combine
+import Core
 
-class HomeViewController: UIViewController {
+class HomeViewController: BaseViewController<HomeView> {
 
+    private let viewModel: RecordListViewModel
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init(viewModel: RecordListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bind()
+        setupDataSource()
+        
+        try? viewModel.fetchRecordList()
+    }
+    
+    // MARK: - Setup
+    private func bind() {
+        viewModel.$records
+            .receive(on: RunLoop.main)
+            .sink { [weak self] records in
+                guard let self else { return }
+                
+                Logger.debug("records: \(records)")
+                self.viewModel.dataSource.update(with: records)
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func setupDataSource() {
+        layoutView.recordCollectionView.delegate = self
+        let dataSource = RecordDiffableDataSource.DataSource(
+            collectionView: layoutView.recordCollectionView,
+            cellProvider: { collectionView, indexPath, item in
+                let cell: RecordCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+                
+                if item.id.isEmpty {
+                    cell.showSkeleton()
+                } else {
+                    cell.hideSkeleton()
+                    cell.configure(imageURL: item.imageURL)
+                }
+                
+                return cell
+            }
+        )
+        
+        dataSource.supplementaryViewProvider = { collecionView, elementKind, indexPath in
+            guard elementKind == UICollectionView.elementKindSectionHeader else { return nil }
+            
+            let headerView = collecionView.dequeueReusableSupplementaryView(
+                ofKind: elementKind,
+                withReuseIdentifier: HeaderView.identifier,
+                for: indexPath) as? HeaderView
+            
+            headerView?.configure(category: "다이어트")
+            headerView?.configure(title: "32회 달성")
+            headerView?.configure(date: "2023-11-03")
+            return headerView
+        }
+        
+        let diffableDataSource = RecordDiffableDataSource(dataSource: dataSource)
+        
+        diffableDataSource.update(with: viewModel.records)
+        
+        viewModel.setupDataSource(diffableDataSource)
     }
 
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? RecordCollectionViewCell else { return }
+        cell.cancelDownloadImage()
+    }
 }
