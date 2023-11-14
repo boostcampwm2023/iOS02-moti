dc10b3791fa4d4ddf8da4d6e5991bcaf2b4de0c5
Dltmd202 <dltmd202@gmail.com>
2023-11-14 22:47:21 +0900

    [BE] fix: transaction-manager 수정
    - 레포지토리 정보를 수정하는 부분 삭제

--- BE/src/config/transaction-manager/transaction-manager.ts
+++ BE/src/config/transaction-manager/transaction-manager.ts
@@ -1,7 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { DiscoveryService, MetadataScanner, Reflector } from '@nestjs/core';
-import { DataSource, Repository } from 'typeorm';
-import { queryRunnerLocalStorage, TRANSACTIONAL_KEY } from './index';
+import { DataSource } from 'typeorm';
+import {
+  queryRunnerLocalStorage,
+  TRANSACTIONAL_KEY,
+  TransactionalMetadata,
+} from './index';
 
 @Injectable()
 export class TransactionManager implements OnModuleInit {
@@ -14,7 +18,6 @@ export class TransactionManager implements OnModuleInit {
 
   onModuleInit(): any {
     this.transactionalWrap();
-    this.repositoryWrap();
   }
 
   transactionalWrap() {
@@ -34,16 +37,18 @@ export class TransactionManager implements OnModuleInit {
       for (const name of names) {
         const originalMethod = instance.instance[name];
 
-        const isTransactional = this.reflector.get(
-          TRANSACTIONAL_KEY,
-          originalMethod,
-        );
+        const transactionOptions: TransactionalMetadata =
+          this.reflector.get<TransactionalMetadata>(
+            TRANSACTIONAL_KEY,
+            originalMethod,
+          );
 
-        if (!isTransactional) {
+        if (!transactionOptions?.isTransactional) {
           continue;
         }
 
         instance.instance[name] = this.wrapMethod(
+          transactionOptions.readonly,
           originalMethod,
           instance.instance,
         );
@@ -51,27 +56,7 @@ export class TransactionManager implements OnModuleInit {
     }
   }
 
-  repositoryWrap() {
-    this.discover
-      .getProviders()
-      .filter((v) => v.isDependencyTreeStatic())
-      .filter(({ metatype, instance }) => {
-        if (!instance || !metatype) return false;
-        else return true;
-      })
-      .filter(({ instance }) => instance instanceof Repository)
-      .forEach(({ instance }) => {
-        Object.defineProperty(instance, 'manager', {
-          configurable: false,
-          get() {
-            const store = queryRunnerLocalStorage.getStore();
-            return store?.queryRunner.manager;
-          },
-        });
-      });
-  }
-
-  wrapMethod(originalMethod: any, instance: any) {
+  wrapMethod(readOnly: boolean, originalMethod: any, instance: any) {
     const { dataSource } = this;
 
     return async function (...args: any[]) {
@@ -81,18 +66,18 @@ export class TransactionManager implements OnModuleInit {
         return await originalMethod.apply(instance, args);
 
       const queryRunner = dataSource.createQueryRunner();
-      await queryRunner.startTransaction();
+      if (!readOnly) await queryRunner.startTransaction();
 
       const result = await queryRunnerLocalStorage.run(
         { queryRunner },
         async () => {
           try {
             const result = await originalMethod.apply(instance, args);
-            await queryRunner.commitTransaction();
+            if (!readOnly) await queryRunner.commitTransaction();
 
             return result;
           } catch (error) {
-            await queryRunner.rollbackTransaction();
+            if (!readOnly) await queryRunner.rollbackTransaction();
             throw error;
           } finally {
             await queryRunner.release();
