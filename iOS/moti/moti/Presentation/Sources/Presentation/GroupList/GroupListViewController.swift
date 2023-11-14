3701c2ea0f1ff4a9b95db9061f0b473d4608fd5a
유정주 <dbwjdwn9216@gmail.com>
2023-11-14 10:27:43 +0900

    [iOS] feat: 화면 이동에 필요한 VC 생성

---
+++ iOS/moti/moti/Presentation/Sources/Presentation/GroupList/GroupListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GroupListViewController.swift
+//  
+//
+//  Created by 유정주 on 11/14/23.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
