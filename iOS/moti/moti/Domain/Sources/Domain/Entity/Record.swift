8381baae51cae24761502b5c8b4a16c9b9d7b4d7
looloolalaa <dlrlgus65@nate.com>
2023-11-14 17:46:52 +0900

    [iOS] feat: 테스트를 위한 MockRecordListRepository 구현

--- iOS/moti/moti/Domain/Sources/Domain/Entity/Record.swift
+++ iOS/moti/moti/Domain/Sources/Domain/Entity/Record.swift
@@ -8,11 +8,21 @@
 import Foundation
 
 public struct Record: Hashable {
-    public let id = UUID()
+    public let id: String
     public let category: String
     public let title: String
     public let imageURL: String
     public let body: String
-    public let achieveCount: Int
-    public let date: Date
+    public let achieveCount: String
+    public let date: String
+    
+    public init(id: String, category: String, title: String, imageURL: String, body: String, achieveCount: String, date: String) {
+        self.id = id
+        self.category = category
+        self.title = title
+        self.imageURL = imageURL
+        self.body = body
+        self.achieveCount = achieveCount
+        self.date = date
+    }
 }
