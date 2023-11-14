58ec2c4186c631aefe09843e444b9d500937e15d
유정주 <dbwjdwn9216@gmail.com>
2023-11-14 10:27:43 +0900

    [iOS] feat: 화면 이동에 필요한 VC 생성

---
+++ iOS/moti/moti/Presentation/Sources/Presentation/Capture/CaptureViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CaptureViewController.swift
+//  
+//
+//  Created by 유정주 on 11/14/23.
+//
+
+import UIKit
+
+class CaptureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
