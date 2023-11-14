750f5c86db689f950e85a86bc9d63cbb968ec4e4
looloolalaa <dlrlgus65@nate.com>
2023-11-14 22:38:54 +0900

    [iOS] refactor: 홈의 라벨에서 사용될 Font 확장 & 자동빌드넘버 커맨드 제거

---
+++ iOS/moti/moti/Design/Sources/Design/Font.swift
@@ -0,0 +1,14 @@
+//
+//  Font.swift
+//
+//
+//  Created by Kihyun Lee on 11/14/23.
+//
+
+import UIKit
+
+public extension UIFont {
+    static let small = UIFont.systemFont(ofSize: 12)
+    static let medium = UIFont.systemFont(ofSize: 24)
+    static let big = UIFont.systemFont(ofSize: 36)
+}
