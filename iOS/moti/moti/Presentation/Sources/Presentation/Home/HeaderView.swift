6b2887cd64723db8492da562bc5c7c73a22c033b
looloolalaa <dlrlgus65@nate.com>
2023-11-14 13:03:58 +0900

    [iOS] fix: 달성 기록 헤더 뷰 수정
    - 2개의 라벨에서 4개의 라벨로 분할

--- iOS/moti/moti/Presentation/Sources/Presentation/Home/HeaderView.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/Home/HeaderView.swift
@@ -10,17 +10,27 @@ import UIKit
 final class HeaderView: UICollectionReusableView {
     
     // MARK: - View
+    private var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24)
+        return label
+    }()
+    
     private var titleLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 2
-        label.text = "다이어트\n32회 달성"
+        label.font = .systemFont(ofSize: 36)
+        return label
+    }()
+    
+    private var dateInfoLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
         return label
     }()
     
     private var dateLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 2
-        label.text = "최근 달성일\n2023-11-03"
+        label.font = .systemFont(ofSize: 12)
         return label
     }()
     
@@ -37,25 +47,45 @@ final class HeaderView: UICollectionReusableView {
     
     // MARK: - Setup
     private func setupUI() {
+        setupCategoryLabel()
         setupTitleLabel()
+        setupDateInfoLabel()
         setupDateLabel()
+    }
+    
+    private func setupCategoryLabel() {
+        addSubview(categoryLabel)
+        categoryLabel.atl
+            .top(equalTo: self.topAnchor, constant: 10)
+            .left(equalTo: self.leftAnchor, constant: 10)
     }
     
     private func setupTitleLabel() {
         addSubview(titleLabel)
         titleLabel.atl
-            .top(equalTo: self.topAnchor, constant: 10)
+            .top(equalTo: categoryLabel.bottomAnchor, constant: 15)
             .left(equalTo: self.leftAnchor, constant: 10)
+    }
+    
+    private func setupDateInfoLabel() {
+        addSubview(dateInfoLabel)
+        dateInfoLabel.atl
+            .top(equalTo: self.topAnchor, constant: 30)
+            .right(equalTo: self.rightAnchor, constant: -20)
     }
     
     private func setupDateLabel() {
         addSubview(dateLabel)
         dateLabel.atl
-            .top(equalTo: self.topAnchor, constant: 30)
+            .top(equalTo: dateInfoLabel.bottomAnchor, constant: 10)
             .right(equalTo: self.rightAnchor, constant: -20)
     }
     
     // MARK: - Method
+    func configure(category: String) {
+        categoryLabel.text = category
+    }
+    
     func configure(title: String) {
         titleLabel.text = title
     }
