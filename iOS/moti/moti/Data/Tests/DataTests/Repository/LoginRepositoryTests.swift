8175bf33ccb3962b116e677375262948d0ef459e
유정주 <dbwjdwn9216@gmail.com>
2023-11-14 15:47:13 +0900

    [iOS] test: Login Repository 실패 케이스 작성

--- iOS/moti/moti/Data/Tests/DataTests/Repository/LoginRepositoryTests.swift
+++ iOS/moti/moti/Data/Tests/DataTests/Repository/LoginRepositoryTests.swift
@@ -11,13 +11,12 @@ import XCTest
 
 final class LoginRepositoryTests: XCTestCase {
 
-    private var repository = MockLoginRepository()
     private let requestValue = LoginRequestValue(identityToken: "eyJraWQiOiJmaDZCczhDIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoia3IuY29kZXNxdWFkLmJvb3N0Y2FtcDgubW90aSIsImV4cCI6MTcwMDAyOTMzNiwiaWF0IjoxNjk5OTQyOTM2LCJzdWIiOiIwMDEzMDYuYTAwZTI5ZGU4N2IyNDgwOGI5N2FiMjlhMDhlMjc3MjAuMTE0MiIsImNfaGFzaCI6InlUOUcwSzRxeDk2R3hrQmxkUkpyZ3ciLCJhdXRoX3RpbWUiOjE2OTk5NDI5MzYsIm5vbmNlX3N1cHBvcnRlZCI6dHJ1ZX0.NFfi63CvYNcTUzc-s3OvrbRjTFDgEF4hJxfKdyjJmOqXUgdpZZhzkUl7-fDRvzpSFxZ8s2D0cxzUoq4hVMt9sWCavWnF7xK67dC9dgiQJ75p_YphVnY22oYUHU3gjBAeGhoziy-NcqeCwD1Sd_bgiwT9aijobYo2C1qVH5ueSgPXcE54Q0luougj960zmCn5WXz3WagO1f4VbnoLVNWZOTgLGZFOrUgHaKxwBtCeh01MC29oSgEcy7VevqfPE7nDI4JDEyN-qIhQGZDOwzvLibH7hIBsLIp0psRtIT_hGNtWfVZLH3ctuwLS4QF-affhI-H6LJUUX6lt_VrLmKpixw")
     private lazy var sourceUserToken = UserToken(accessToken: "testAccessToken", refreshToken: "testRefreshToken", user: sourceUser)
     private let sourceUser = User(code: "ABCDEFG", avatarURL: URL(string: "https://test.com"))
 
     override func setUpWithError() throws {
-        repository = MockLoginRepository()
+
     }
 
     override func tearDownWithError() throws {
@@ -25,6 +24,7 @@ final class LoginRepositoryTests: XCTestCase {
     }
 
     func test_형식에_맞는_json을_디코딩하면_userToken_반환() throws {
+        let repository = MockLoginRepository()
         let expectation = XCTestExpectation(description: "test_형식에_맞는_json을_디코딩하면_userToken_반환")
         
         Task {
@@ -39,7 +39,32 @@ final class LoginRepositoryTests: XCTestCase {
         wait(for: [expectation], timeout: 3)
     }
     
-    func test_형식에_맞지_않는_json을_디코딩하면_decode_에러() throws {
+    func test_형식에_맞지_않는_json을_디코딩하면_nil() throws {
+        // data를 감싸는 닫는 중괄호가 없음
+        let failedJSON = """
+        {
+            "success": true,
+            "data": {
+                "accessToken": "testAccessToken",
+                "refreshToken": "testRefreshToken",
+                "user": {
+                    "userCode": "ABCDEFG",
+                    "avatar_url": "https://test.com",
+                }
+            
+        }
+        """
+        let repository = MockLoginRepository(json: failedJSON)
+        let expectation = XCTestExpectation(description: "test_형식에_맞지_않는_json을_디코딩하면_nil")
         
+        Task {
+            // TODO: Error 체크 어떻게 하는지 확인
+            let result = try? await repository.login(requestValue: requestValue)
+            XCTAssertNil(result)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3)
     }
 }
