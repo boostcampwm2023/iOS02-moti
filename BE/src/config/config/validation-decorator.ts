a914407c59bb13779c23746661028950f9c313c0
Dltmd202 <dltmd202@gmail.com>
2023-11-14 15:29:31 +0900

    [BE] fix: MotiPolicyCreate 수정
    - Validation 과정에서 에러가 단일메시지 형태로 나가도록 수정

---
+++ BE/src/config/config/validation-decorator.ts
@@ -0,0 +1,18 @@
+import { registerDecorator, ValidationOptions } from 'class-validator';
+
+export const IsNotEmptyString = (validationOptions?: ValidationOptions) => {
+  return function (object: object, propertyKey: string) {
+    registerDecorator({
+      name: 'isNonEmptyString',
+      target: object.constructor,
+      propertyName: propertyKey,
+      constraints: [],
+      options: validationOptions,
+      validator: {
+        validate(value: any) {
+          return value !== undefined && typeof value === 'string';
+        },
+      },
+    });
+  };
+};
