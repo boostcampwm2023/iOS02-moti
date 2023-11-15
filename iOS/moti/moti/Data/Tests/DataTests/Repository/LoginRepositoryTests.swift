f2e58596c3edd0e164abdbb39f3a248018b4af9c
유정주 <dbwjdwn9216@gmail.com>
2023-11-15 21:58:19 +0900

    [iOS] test: 자동 로그인 Repository 테스트

--- iOS/moti/moti/Data/Tests/DataTests/Repository/LoginRepositoryTests.swift
+++ iOS/moti/moti/Data/Tests/DataTests/Repository/LoginRepositoryTests.swift
@@ -11,7 +11,8 @@ import XCTest
 
 final class LoginRepositoryTests: XCTestCase {
 
-    private let requestValue = LoginRequestValue(identityToken: "eyJraWQiOiJmaDZCczhDIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoia3IuY29kZXNxdWFkLmJvb3N0Y2FtcDgubW90aSIsImV4cCI6MTcwMDAyOTMzNiwiaWF0IjoxNjk5OTQyOTM2LCJzdWIiOiIwMDEzMDYuYTAwZTI5ZGU4N2IyNDgwOGI5N2FiMjlhMDhlMjc3MjAuMTE0MiIsImNfaGFzaCI6InlUOUcwSzRxeDk2R3hrQmxkUkpyZ3ciLCJhdXRoX3RpbWUiOjE2OTk5NDI5MzYsIm5vbmNlX3N1cHBvcnRlZCI6dHJ1ZX0.NFfi63CvYNcTUzc-s3OvrbRjTFDgEF4hJxfKdyjJmOqXUgdpZZhzkUl7-fDRvzpSFxZ8s2D0cxzUoq4hVMt9sWCavWnF7xK67dC9dgiQJ75p_YphVnY22oYUHU3gjBAeGhoziy-NcqeCwD1Sd_bgiwT9aijobYo2C1qVH5ueSgPXcE54Q0luougj960zmCn5WXz3WagO1f4VbnoLVNWZOTgLGZFOrUgHaKxwBtCeh01MC29oSgEcy7VevqfPE7nDI4JDEyN-qIhQGZDOwzvLibH7hIBsLIp0psRtIT_hGNtWfVZLH3ctuwLS4QF-affhI-H6LJUUX6lt_VrLmKpixw")
+    private let loginRequestValue = LoginRequestValue(identityToken: "test")
+    private let autoLoginRequestValue = AutoLoginRequestValue(refreshToken: "test")
     private lazy var sourceUserToken = UserToken(accessToken: "testAccessToken", refreshToken: "testRefreshToken", user: sourceUser)
     private let sourceUser = User(code: "ABCDEFG", avatarURL: URL(string: "https://test.com"))
 
@@ -28,7 +29,7 @@ final class LoginRepositoryTests: XCTestCase {
         let expectation = XCTestExpectation(description: "test_형식에_맞는_json을_디코딩하면_userToken_반환")
         
         Task {
-            let result = try await repository.login(requestValue: requestValue)
+            let result = try await repository.login(requestValue: loginRequestValue)
         
             XCTAssertEqual(result, sourceUserToken)
             XCTAssertEqual(result.user, sourceUser)
@@ -59,8 +60,24 @@ final class LoginRepositoryTests: XCTestCase {
         
         Task {
             // TODO: Error 체크 어떻게 하는지 확인
-            let result = try? await repository.login(requestValue: requestValue)
+            let result = try? await repository.login(requestValue: loginRequestValue)
             XCTAssertNil(result)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3)
+    }
+    
+    func test_형식에_맞는_autologin_json을_디코딩하면_userToken_반환() throws {
+        let repository = MockLoginRepository()
+        let expectation = XCTestExpectation(description: "test_형식에_맞는_json을_디코딩하면_userToken_반환")
+        
+        Task {
+            let result = try await repository.autoLogin(requestValue: autoLoginRequestValue)
+        
+            XCTAssertEqual(result, sourceUserToken)
+            XCTAssertEqual(result.user, sourceUser)
             
             expectation.fulfill()
         }
