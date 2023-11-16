414b442a0feb3321b946f027139e4a12f7b44e0f
Dltmd202 <dltmd202@gmail.com>
2023-11-16 19:55:42 +0900

    [BE] feat: AdminRepository 생성
    - saveAdmin admin 저장
    - getUserAdmin 해당 유저에 대한 admin 조회
    - findActiveAdminByEmail email에 대한 ACTIVE admin 조회

---
+++ BE/src/admin/entities/admin.repository.ts
@@ -0,0 +1,32 @@
+import { CustomRepository } from '../../config/typeorm/custom-repository.decorator';
+import { AdminEntity } from './admin.entity';
+import { TransactionalRepository } from '../../config/transaction-manager/transactional-repository';
+import { Admin } from '../domain/admin.domain';
+import { User } from '../../users/domain/user.domain';
+import { UserEntity } from '../../users/entities/user.entity';
+import { AdminStatus } from '../domain/admin-status';
+
+@CustomRepository(AdminEntity)
+export class AdminRepository extends TransactionalRepository<AdminEntity> {
+  async saveAdmin(admin: Admin): Promise<Admin> {
+    const adminEntity = AdminEntity.from(admin);
+    const savedAdminEntity = await this.repository.save(adminEntity);
+    return savedAdminEntity?.toModel();
+  }
+
+  async getUserAdmin(user: User): Promise<Admin> {
+    const userEntity = UserEntity.from(user);
+    const adminEntity: AdminEntity = await this.repository.findOne({
+      where: { user: { id: userEntity.id } },
+    });
+    return adminEntity?.toModel();
+  }
+
+  async findActiveAdminByEmail(email: string): Promise<Admin> {
+    const adminEntity = await this.findOne({
+      where: { email: email, status: AdminStatus.ACTIVE },
+      relations: ['user'],
+    });
+    return adminEntity?.toModel();
+  }
+}
