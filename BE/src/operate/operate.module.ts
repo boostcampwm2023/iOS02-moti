97e86e781e01cdf0b94ea00863d939bd4fac6860
Dltmd202 <dltmd202@gmail.com>
2023-11-16 23:26:18 +0900

    [BE] chore: 운영 MVC 컨트롤러 생성

--- BE/src/operate/operate.module.ts
+++ BE/src/operate/operate.module.ts
@@ -3,9 +3,10 @@ import { OperateController } from './controller/operate.controller';
 import { OperateService } from './application/operate.service';
 import { CustomTypeOrmModule } from '../config/typeorm/custom-typeorm.module';
 import { MotiPolicyRepository } from './entities/moti-policy.repository';
+import { OperateMvcController } from './controller/operate-mvc.controller';
 
 @Module({
-  controllers: [OperateController],
+  controllers: [OperateController, OperateMvcController],
   providers: [OperateService],
   imports: [CustomTypeOrmModule.forCustomRepository([MotiPolicyRepository])],
 })
