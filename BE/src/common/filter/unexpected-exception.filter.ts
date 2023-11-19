2e8e6bfc0a0ae48eb8061d0587ca46ad4ece9980
Dltmd202 <dltmd202@gmail.com>
2023-11-19 22:17:10 +0900

    [BE] refactor: UnexpectedExceptionFilter에서 로깅

--- BE/src/common/filter/unexpected-exception.filter.ts
+++ BE/src/common/filter/unexpected-exception.filter.ts
@@ -1,12 +1,15 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiData } from '../api/api-data';
 
 @Catch()
 export class UnexpectedExceptionFilter implements ExceptionFilter {
+  logger = new Logger(UnexpectedExceptionFilter.name);
+
   catch(exception: unknown, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+    this.logger.error(exception);
 
     response.status(500).json(ApiData.error('Internal Server Error'));
   }
