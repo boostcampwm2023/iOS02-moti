0ded7d369361a852df73bcb31145b0d4f8d641fe
Dltmd202 <dltmd202@gmail.com>
2023-11-19 22:20:50 +0900

    [BE] refactor: UserAlreadyRegisteredAdminException 이름 수정

--- BE/src/admin/exception/user-already-registered-admin.exception.ts
+++ BE/src/admin/exception/user-already-registered-admin.exception.ts
@@ -1,7 +1,7 @@
 import { MotimateException } from '../../common/exception/motimate.excpetion';
 import { ERROR_INFO } from '../../common/exception/error-code';
 
-export class UserAlreadyRegisteredAdmin extends MotimateException {
+export class UserAlreadyRegisteredAdminException extends MotimateException {
   constructor() {
     super(ERROR_INFO.USER_ALREADY_REGISTERED_ADMIN);
   }
