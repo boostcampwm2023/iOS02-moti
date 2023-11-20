bc5aabf249b7786d80085b2294878a06366cc3c2
유정주 <dbwjdwn9216@gmail.com>
2023-11-20 20:52:59 +0900

    [iOS] feat: 카테고리 선택 pickerView 추가

--- iOS/moti/moti/Presentation/Sources/Presentation/CaptureResult/CaptureResultViewController.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/CaptureResult/CaptureResultViewController.swift
@@ -10,10 +10,12 @@ import Core
 import Design
 
 final class CaptureResultViewController: BaseViewController<CaptureResultView> {
-
     // MARK: - Properties
     weak var coordinator: CaptureResultCoordinator?
+    
+    // TODO: ViewModel로 변환
     private let resultImageData: Data
+    private let categories: [String] = ["카테고리1", "카테고리2", "카테고리3", "카테고리4", "카테고리5"]
     
     // MARK: - Init
     init(resultImageData: Data) {
@@ -28,12 +30,32 @@ final class CaptureResultViewController: BaseViewController<CaptureResultView> {
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPickerView()
+        addTarget()
         
         if let resultImage = convertDataToImage(resultImageData) {
             layoutView.configure(image: resultImage, count: 10)
         } else {
             layoutView.configure(image: MotiImage.sample1, count: 10)
         }
+    }
+    
+    private func setupPickerView() {
+        layoutView.categoryPickerView.delegate = self
+        layoutView.categoryPickerView.dataSource = self
+    }
+    
+    private func addTarget() {
+        layoutView.categoryButton.addTarget(self, action: #selector(showPicker), for: .touchUpInside)
+        layoutView.selectDoneButton.addTarget(self, action: #selector(donePicker), for: .touchUpInside)
+    }
+    
+    @objc func showPicker() {
+        layoutView.showCategoryPicker()
+    }
+    
+    @objc func donePicker() {
+        layoutView.hideCategoryPicker()
     }
     
     private func convertDataToImage(_ data: Data) -> UIImage? {
@@ -53,5 +75,25 @@ final class CaptureResultViewController: BaseViewController<CaptureResultView> {
         
         let croppedImage = UIImage(cgImage: imageRef)
         return croppedImage
+    }
+}
+
+extension CaptureResultViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        layoutView.update(category: categories[row])
+    }
+}
+
+extension CaptureResultViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
     }
 }
