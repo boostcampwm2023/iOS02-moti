07bfde3efa5c8924968bce8d7ee7fe436c2fc114
lsh23 <shseoul14@gmail.com>
2023-11-20 22:52:16 +0900

    [BE] refactor: 조건문을 간결하게 리팩토링한다.

--- BE/src/achievement/entities/achievement.repository.ts
+++ BE/src/achievement/entities/achievement.repository.ts
@@ -14,10 +14,7 @@ export class AchievementRepository extends TransactionalRepository<AchievementEn
     const where: FindOptionsWhere<AchievementEntity> = {
       user: { id: userId },
     };
-    if (
-      achievementPaginationOption.categoryId &&
-      achievementPaginationOption.categoryId !== 0
-    ) {
+    if (achievementPaginationOption?.categoryId !== 0) {
       where.category = { id: achievementPaginationOption.categoryId };
     }
     if (achievementPaginationOption.whereIdLessThan) {
