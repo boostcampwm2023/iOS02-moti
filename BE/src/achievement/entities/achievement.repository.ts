0fb60acbeefaee4f6c2ba36b00e7cc346c429c73
lsh23 <shseoul14@gmail.com>
2023-11-21 23:46:59 +0900

    [BE] feat: 달성기록 상세 정보을 조회하는 쿼리를 query builder로 작성한다.

--- BE/src/achievement/entities/achievement.repository.ts
+++ BE/src/achievement/entities/achievement.repository.ts
@@ -4,6 +4,8 @@ import { AchievementEntity } from './achievement.entity';
 import { Achievement } from '../domain/achievement.domain';
 import { FindOptionsWhere, LessThan } from 'typeorm';
 import { PaginateAchievementRequest } from '../dto/paginate-achievement-request';
+import { AchievementDetailResponse } from '../dto/achievement-detail-response';
+import { IAchievementDetail } from '../index';
 
 @CustomRepository(AchievementEntity)
 export class AchievementRepository extends TransactionalRepository<AchievementEntity> {
@@ -34,5 +36,34 @@ export class AchievementRepository extends TransactionalRepository<AchievementEn
     const achievementEntity = AchievementEntity.from(achievement);
     const saved = await this.repository.save(achievementEntity);
     return saved.toModel();
+  }
+
+  async findAchievementDetail(userId: number, achievementId: number) {
+    const result = await this.repository
+      .createQueryBuilder('achievement')
+      .leftJoinAndSelect('achievement.category', 'category')
+      .select([
+        'achievement.id as id',
+        'achievement.title as title',
+        'achievement.content as content',
+        'achievement.imageUrl as imageUrl',
+        'achievement.created_at as createdAt',
+        'category.id as categoryId',
+        'category.name as categoryName',
+      ])
+      .addSelect(['COUNT(a.id) as round'])
+      .leftJoin(
+        'achievement',
+        'a',
+        'a.category_id = achievement.category_id AND a.id <= achievement.id',
+      )
+      .where('achievement.id = :achievementId', { achievementId })
+      .andWhere('achievement.user_id = :userId', { userId })
+      .getRawOne<IAchievementDetail>();
+
+    if (result.id) {
+      return new AchievementDetailResponse(result);
+    }
+    return null;
   }
 }
