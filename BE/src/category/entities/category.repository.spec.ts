de8b5abc85ce97c9e66f25d02d7fb9bafa340a75
Dltmd202 <dltmd202@gmail.com>
2023-11-21 14:44:41 +0900

    [BE] feat: 카테고리 레포지토리에 findByUserWithCount 추가
    - findByUserWithCount는 사용자에 대한 카테고리 정보들을 추출하는 메서드

--- BE/src/category/entities/category.repository.spec.ts
+++ BE/src/category/entities/category.repository.spec.ts
@@ -6,15 +6,21 @@ import { typeOrmModuleOptions } from '../../config/typeorm';
 import { OperateModule } from '../../operate/operate.module';
 import { ConfigModule } from '@nestjs/config';
 import { configServiceModuleOptions } from '../../config/config';
-import { CategoryModule } from '../category.module';
 import { UsersFixture } from '../../../test/user/users-fixture';
 import { Category } from '../domain/category.domain';
 import { UsersTestModule } from '../../../test/user/users-test.module';
+import { transactionTest } from '../../../test/common/transaction-test';
+import { AchievementFixture } from '../../../test/achievement/achievement-fixture';
+import { AchievementTestModule } from '../../../test/achievement/achievement-test.module';
+import { CategoryTestModule } from '../../../test/category/category-test.module';
+import { CategoryFixture } from '../../../test/category/category-fixture';
 
 describe('CategoryRepository', () => {
   let categoryRepository: CategoryRepository;
+  let categoryFixture: CategoryFixture;
   let dataSource: DataSource;
   let usersFixture: UsersFixture;
+  let achievementFixture: AchievementFixture;
 
   beforeAll(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -23,12 +29,15 @@ describe('CategoryRepository', () => {
         UsersTestModule,
         OperateModule,
         ConfigModule.forRoot(configServiceModuleOptions),
-        CategoryModule,
+        CategoryTestModule,
+        AchievementTestModule,
       ],
       controllers: [],
       providers: [],
     }).compile();
 
+    categoryFixture = app.get<CategoryFixture>(CategoryFixture);
+    achievementFixture = app.get<AchievementFixture>(AchievementFixture);
     usersFixture = app.get<UsersFixture>(UsersFixture);
     categoryRepository = app.get<CategoryRepository>(CategoryRepository);
     dataSource = app.get<DataSource>(DataSource);
@@ -45,31 +54,78 @@ describe('CategoryRepository', () => {
   });
 
   it('saveCategory는 카테고리를 생성할 수 있다.', async () => {
-    // given
-    const user = await usersFixture.getUser('ABC');
-    const category = new Category(user, '카테고리1');
+    await transactionTest(dataSource, async () => {
+      // given
+      const user = await usersFixture.getUser('ABC');
+      const category = new Category(user, '카테고리1');
 
-    // when
-    const savedCategory = await categoryRepository.saveCategory(category);
+      // when
+      const savedCategory = await categoryRepository.saveCategory(category);
 
-    // then
-    expect(savedCategory.name).toBe('카테고리1');
-    expect(savedCategory.user).toEqual(user);
+      // then
+      expect(savedCategory.name).toBe('카테고리1');
+      expect(savedCategory.user).toEqual(user);
+    });
   });
 
   it('findById는 id로 user를 제외된 카테고리를 조회할 수 있다.', async () => {
-    // given
-    const user = await usersFixture.getUser(1);
-    const category = new Category(user, '카테고리1');
-    const savedCategory = await categoryRepository.saveCategory(category);
+    await transactionTest(dataSource, async () => {
+      // given
+      const user = await usersFixture.getUser(1);
+      const category = new Category(user, '카테고리1');
+      const savedCategory = await categoryRepository.saveCategory(category);
 
-    // when
-    const retrievedCategory = await categoryRepository.findById(
-      savedCategory.id,
-    );
+      // when
+      const retrievedCategory = await categoryRepository.findById(
+        savedCategory.id,
+      );
 
-    // then
-    expect(retrievedCategory.name).toBe('카테고리1');
-    expect(retrievedCategory.user).toBeUndefined();
+      // then
+      expect(retrievedCategory.name).toBe('카테고리1');
+      expect(retrievedCategory.user).toBeUndefined();
+    });
+  });
+
+  it('findByUserWithCount는 사용자가 소유한 카테고리가 없으면 빈 배열을 반환한다.', async () => {
+    await transactionTest(dataSource, async () => {
+      const user = await usersFixture.getUser(1);
+
+      const retrievedCategories =
+        await categoryRepository.findByUserWithCount(user);
+
+      expect(retrievedCategories).toBeDefined();
+      expect(retrievedCategories.length).toBe(0);
+    });
+  });
+
+  it('findByUserWithCount', async () => {
+    await transactionTest(dataSource, async () => {
+      const user = await usersFixture.getUser(1);
+      const categories: Category[] = await categoryFixture.getCategories(
+        4,
+        user,
+      );
+      await achievementFixture.getAchievements(4, user, categories[0]);
+      await achievementFixture.getAchievements(5, user, categories[1]);
+      await achievementFixture.getAchievements(7, user, categories[2]);
+      await achievementFixture.getAchievements(10, user, categories[3]);
+
+      const retrievedCategories =
+        await categoryRepository.findByUserWithCount(user);
+
+      expect(retrievedCategories.length).toBe(4);
+      expect(retrievedCategories[0].categoryId).toEqual(categories[0].id);
+      expect(retrievedCategories[0].insertedAt).toBeInstanceOf(Date);
+      expect(retrievedCategories[0].achievementCount).toBe(4);
+      expect(retrievedCategories[1].categoryId).toEqual(categories[1].id);
+      expect(retrievedCategories[1].insertedAt).toBeInstanceOf(Date);
+      expect(retrievedCategories[1].achievementCount).toBe(5);
+      expect(retrievedCategories[2].categoryId).toEqual(categories[2].id);
+      expect(retrievedCategories[2].insertedAt).toBeInstanceOf(Date);
+      expect(retrievedCategories[2].achievementCount).toBe(7);
+      expect(retrievedCategories[3].categoryId).toEqual(categories[3].id);
+      expect(retrievedCategories[3].insertedAt).toBeInstanceOf(Date);
+      expect(retrievedCategories[3].achievementCount).toBe(10);
+    });
   });
 });
