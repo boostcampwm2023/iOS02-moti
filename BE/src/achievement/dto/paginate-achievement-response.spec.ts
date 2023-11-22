85de8fcc97692dbb2e2dfe373c0c39e9ec8d6f90
lsh23 <shseoul14@gmail.com>
2023-11-23 00:01:57 +0900

    [BE] fix: categoryId 0인 경우에도 next에 넣어주도록 수정한다.

--- BE/src/achievement/dto/paginate-achievement-response.spec.ts
+++ BE/src/achievement/dto/paginate-achievement-response.spec.ts
@@ -20,6 +20,7 @@ describe('PaginateAchievementResponse test', () => {
     );
     expect(response.next).toEqual({ take: 12, whereIdLessThan: 88 });
   });
+
   test('다음 페이지 네이션 요청을 위한 정보를 가지고 있는 next을 생성한다. - categoryId 필터링 추가', () => {
     const paginateAchievementRequest = new PaginateAchievementRequest();
     paginateAchievementRequest.take = 12;
@@ -30,6 +31,21 @@ describe('PaginateAchievementResponse test', () => {
     );
     expect(response.next).toEqual({
       categoryId: 1,
+      take: 12,
+      whereIdLessThan: 88,
+    });
+  });
+
+  test('다음 페이지 네이션 요청을 위한 정보를 가지고 있는 next을 생성한다. - categoryId가 0인 경우', () => {
+    const paginateAchievementRequest = new PaginateAchievementRequest();
+    paginateAchievementRequest.take = 12;
+    paginateAchievementRequest.categoryId = 0;
+    const response = new PaginateAchievementResponse(
+      paginateAchievementRequest,
+      achievementResponses.slice(0, paginateAchievementRequest.take),
+    );
+    expect(response.next).toEqual({
+      categoryId: 0,
       take: 12,
       whereIdLessThan: 88,
     });
