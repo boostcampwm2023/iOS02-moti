f926886e8507a215ee8fa1f52f72e9e938b67af4
Dltmd202 <dltmd202@gmail.com>
2023-11-22 17:36:18 +0900

    [BE] feat: CategoryResponse 수정
    - id 프로퍼티를 추가함

--- BE/src/category/dto/category.response.ts
+++ BE/src/category/dto/category.response.ts
@@ -2,14 +2,18 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Category } from '../domain/category.domain';
 
 export class CategoryResponse {
+  @ApiProperty({ description: '카테고리 아이디' })
+  id: number;
+
   @ApiProperty({ description: '카테고리 이름' })
   name: string;
 
-  constructor(name: string) {
+  constructor(id: number, name: string) {
+    this.id = id;
     this.name = name;
   }
 
   static from(category: Category) {
-    return new CategoryResponse(category.name);
+    return new CategoryResponse(category.id, category.name);
   }
 }
