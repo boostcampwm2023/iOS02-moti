229484852b596d8e48cf1f54d07fc798e7019e71
Dltmd202 <dltmd202@gmail.com>
2023-11-23 13:29:12 +0900

    [BE] feat: LocalFileStore 테스트
    - 저장 테스트
    - 저장 실패 테스트
    - 삭제 테스트
    - 삭제 싪패 테스트

---
+++ BE/test/common/file-store/file-fixture.ts
@@ -0,0 +1,18 @@
+import { File } from '../../../src/common/application/file-store';
+
+export class FileFixture {
+  static file(name: string, extension: string): File {
+    return {
+      originalname: `${name}.${extension}`,
+      buffer: Buffer.from(`${name}.${extension}`.repeat(100)),
+      encoding: 'utf-8',
+      fieldname: 'test',
+      mimetype: 'image/jpeg',
+      size: 100,
+      destination: 'test',
+      filename: 'test.jpg',
+      path: 'test',
+      stream: null,
+    };
+  }
+}
