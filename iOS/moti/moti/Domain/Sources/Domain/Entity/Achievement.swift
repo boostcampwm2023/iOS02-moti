9ef55dde3ddd094e156a5bbbe2bc24614a1e2a68
looloolalaa <dlrlgus65@nate.com>
2023-11-23 16:42:33 +0900

    [iOS] refactor: Achievement 속성 수정

--- iOS/moti/moti/Domain/Sources/Domain/Entity/Achievement.swift
+++ iOS/moti/moti/Domain/Sources/Domain/Entity/Achievement.swift
@@ -9,19 +9,19 @@ import Foundation
 
 public struct Achievement: Hashable {
     public let id: Int
-    public let category: String?
+    public let category: String
     public let title: String
     public let imageURL: URL?
     public let body: String?
-    public let achieveCount: Int?
-    public let date: Date?
+    public let achieveCount: Int
+    public let date: Date
     
     public init(
         id: Int,
         category: String,
         title: String,
         imageURL: URL?,
-        body: String,
+        body: String?,
         achieveCount: Int,
         date: Date
     ) {
@@ -36,11 +36,11 @@ public struct Achievement: Hashable {
     
     public init(id: Int, title: String, imageURL: URL?) {
         self.id = id
-        self.category = nil
+        self.category = ""
         self.title = title
         self.imageURL = imageURL
         self.body = nil
-        self.achieveCount = nil
-        self.date = nil
+        self.achieveCount = 0
+        self.date = Date()
     }
 }
