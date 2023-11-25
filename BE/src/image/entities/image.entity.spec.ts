99e65281c2cbc8f2da764229da06d7e25dd2f6d5
Dltmd202 <dltmd202@gmail.com>
2023-11-25 18:02:46 +0900

    [BE] feat: ImageEntity 생성
    - typeorm 종속 객체
    - 변환 메서드 테스트 코드

---
+++ BE/src/image/entities/image.entity.spec.ts
@@ -0,0 +1,44 @@
+import { Image } from '../domain/image.domain';
+import { ImageEntity } from './image.entity';
+import { User } from '../../users/domain/user.domain';
+
+describe('ImageEntity Test', () => {
+  describe('from으로 Image에 대한 ImageEntity를 만들 수 있다.', () => {
+    it('achievement가 비어있는 경우에도 from을 사용할 수 있다.', () => {
+      // given
+      const user = new User();
+      const image = new Image(user, 'originalName', 'imageUrl');
+
+      // when
+      const imageEntity = ImageEntity.from(image);
+
+      // then
+      expect(imageEntity).toBeInstanceOf(ImageEntity);
+      expect(imageEntity.id).toBe(image.id);
+      expect(imageEntity.originalName).toBe(image.originalName);
+      expect(imageEntity.imageUrl).toBe(image.imageUrl);
+      expect(imageEntity.thumbnailUrl).toBeUndefined();
+      expect(imageEntity.achievement).toBeUndefined();
+    });
+  });
+
+  describe('toModel으로 ImageEntity를 Imaeg 도메인 객체로 변환할 수 있다.', () => {
+    it('achievemnt가 비어있는 경우에도 toModel로 변환할 수 있다.', () => {
+      // given
+      const user = new User();
+      const image = new Image(user, 'originalName', 'imageUrl');
+
+      // when
+      const imageEntity = ImageEntity.from(image);
+      const result = imageEntity.toModel();
+
+      // then
+      expect(result).toBeInstanceOf(Image);
+      expect(result.id).toBe(image.id);
+      expect(result.originalName).toBe(image.originalName);
+      expect(result.imageUrl).toBe(image.imageUrl);
+      expect(result.thumbnailUrl).toBeUndefined();
+      expect(result.achievement).toBeUndefined();
+    });
+  });
+});
