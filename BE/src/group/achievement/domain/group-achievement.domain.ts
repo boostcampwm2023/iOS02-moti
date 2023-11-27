c244ed6bb458e5e71d0a20a240c2fb64257d992c
Dltmd202 <dltmd202@gmail.com>
2023-11-28 00:09:32 +0900

    [BE] feat: GroupAchievement 엔티티, 도메인, fixture

---
+++ BE/src/group/achievement/domain/group-achievement.domain.ts
@@ -0,0 +1,26 @@
+import { GroupCategory } from '../../category/domain/group.category';
+import { Group } from '../../group/domain/group.domain';
+import { User } from '../../../users/domain/user.domain';
+
+export class GroupAchievement {
+  id: number;
+  title: string;
+  user: User;
+  group: Group;
+  groupCategory: GroupCategory;
+  content: string;
+
+  constructor(
+    title: string,
+    user: User,
+    group: Group,
+    groupCategory: GroupCategory,
+    content: string,
+  ) {
+    this.title = title;
+    this.user = user;
+    this.group = group;
+    this.groupCategory = groupCategory;
+    this.content = content;
+  }
+}
---
+++ BE/src/group/achievement/entities/group-achievement.entity.ts
@@ -0,0 +1,65 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { BaseTimeEntity } from '../../../common/entities/base.entity';
+import { UserEntity } from '../../../users/entities/user.entity';
+import { GroupCategoryEntity } from '../../category/entities/group-category.entity';
+import { GroupEntity } from '../../group/entities/group.entity';
+import { GroupAchievement } from '../domain/group-achievement.domain';
+
+@Entity({ name: 'group_achievement' })
+export class GroupAchievementEntity extends BaseTimeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  title: string;
+
+  @ManyToOne(() => UserEntity)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  user: UserEntity;
+
+  @ManyToOne(() => GroupEntity)
+  @JoinColumn({ name: 'group_id', referencedColumnName: 'id' })
+  group: GroupEntity;
+
+  @ManyToOne(() => GroupCategoryEntity)
+  @JoinColumn({ name: 'group_category_id', referencedColumnName: 'id' })
+  groupCategory: GroupCategoryEntity;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  toModel(): GroupAchievement {
+    const groupAchievement = new GroupAchievement(
+      this.title,
+      this.user?.toModel() || null,
+      this.group?.toModel() || null,
+      this.groupCategory?.toModel() || null,
+      this.content,
+    );
+    groupAchievement.id = this.id;
+    return groupAchievement;
+  }
+
+  static from(groupAchievement: GroupAchievement): GroupAchievementEntity {
+    const groupAchievementEntity = new GroupAchievementEntity();
+    groupAchievementEntity.id = groupAchievement.id;
+    groupAchievementEntity.title = groupAchievement.title;
+    groupAchievementEntity.user = groupAchievement.user
+      ? UserEntity.from(groupAchievement.user)
+      : null;
+    groupAchievementEntity.group = groupAchievement.group
+      ? GroupEntity.from(groupAchievement.group)
+      : null;
+    groupAchievementEntity.groupCategory = groupAchievement.groupCategory
+      ? GroupCategoryEntity.from(groupAchievement.groupCategory)
+      : null;
+    groupAchievementEntity.content = groupAchievement.content;
+    return groupAchievementEntity;
+  }
+}
