7575db55c1901efc2cdf88613307a05b171c2f43
Dltmd202 <dltmd202@gmail.com>
2023-11-27 15:28:07 +0900

    [BE] feat: AuthFixture 업데이트
    - 어드민 계정의 토큰을 받을 수 있도록 getAuthenticatedAdmin 추가

--- BE/test/auth/auth-test.module.ts
+++ BE/test/auth/auth-test.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersTestModule } from '../user/users-test.module';
 import { AuthModule } from '../../src/auth/auth.module';
 import { AuthFixture } from './auth-fixture';
+import { AdminTestModule } from '../admin/admin-test.module';
 
 @Module({
-  imports: [UsersTestModule, AuthModule],
+  imports: [UsersTestModule, AuthModule, AdminTestModule],
   exports: [AuthFixture],
   providers: [AuthFixture],
 })
