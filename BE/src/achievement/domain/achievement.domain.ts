47d4700988025b75c85f12fe2aeadd9a5680d4af
Dltmd202 <dltmd202@gmail.com>
2023-11-29 00:21:48 +0900

    [BE] fix: Achievement 도메인 업데이트

--- BE/src/achievement/domain/achievement.domain.ts
+++ BE/src/achievement/domain/achievement.domain.ts
@@ -1,6 +1,7 @@
 import { User } from '../../users/domain/user.domain';
 import { Category } from '../../category/domain/category.domain';
 import { AchievementUpdate } from '../index';
+import { Image } from '../../image/domain/image.domain';
 
 export class Achievement {
   id: number;
@@ -13,32 +14,20 @@ export class Achievement {
 
   content: string;
 
-  imageUrl: string;
-
-  thumbnailUrl: string;
+  image: Image;
 
   constructor(
     user: User,
     category: Category,
     title: string,
     content: string,
-    imageUrl: string,
-    thumbnailUrl: string,
+    image: Image,
   ) {
     this.user = user;
     this.category = category;
     this.title = title;
     this.content = content;
-    this.imageUrl = imageUrl;
-    this.thumbnailUrl = thumbnailUrl;
-  }
-
-  assignUser(user: User) {
-    this.user = user;
-  }
-
-  assignCategory(category: Category) {
-    this.category = category;
+    this.image = image;
   }
 
   update(achievementUpdate: AchievementUpdate) {
