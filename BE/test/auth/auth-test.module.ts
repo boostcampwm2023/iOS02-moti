13e0d6c014aa9f1234f9cfd42f873cfe722265d0
lsh23 <shseoul14@gmail.com>
2023-11-28 14:14:06 +0900

    [BE] test: e2e test를 위한 StubOauthHandler를 추가한다.

--- BE/test/auth/auth-test.module.ts
+++ BE/test/auth/auth-test.module.ts
@@ -3,10 +3,12 @@ import { UsersTestModule } from '../user/users-test.module';
 import { AuthModule } from '../../src/auth/auth.module';
 import { AuthFixture } from './auth-fixture';
 import { AdminTestModule } from '../admin/admin-test.module';
+import { StubOauthHandler } from './StubOauthHandler';
+import { E2EAuthFixture } from './auth-e2e-fixture';
 
 @Module({
   imports: [UsersTestModule, AuthModule, AdminTestModule],
   exports: [AuthFixture],
-  providers: [AuthFixture],
+  providers: [AuthFixture, StubOauthHandler, E2EAuthFixture],
 })
 export class AuthTestModule {}
