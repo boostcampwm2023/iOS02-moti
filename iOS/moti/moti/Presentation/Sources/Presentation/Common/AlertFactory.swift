d3a7a5ee86a9355d09e5da187b55323a3d104fc2
looloolalaa <dlrlgus65@nate.com>
2023-11-28 11:48:30 +0900

    [iOS] refactor: showAlert in BaseVC

--- iOS/moti/moti/Presentation/Sources/Presentation/Common/AlertFactory.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/Common/AlertFactory.swift
@@ -8,7 +8,22 @@
 import UIKit
 
 public enum AlertFactory {
-    public static func makeNormalAlert(
+    public static func makeOneButtonAlert(
+        title: String? = nil,
+        message: String? = nil,
+        okTitle: String? = "확인",
+        okAction: (() -> Void)? = nil
+    ) -> UIAlertController {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAlert = UIAlertAction(title: okTitle, style: .default) { _ in
+            okAction?()
+        }
+        
+        alertVC.addAction(okAlert)
+        return alertVC
+    }
+    
+    public static func makeTwoButtonAlert(
         title: String? = nil,
         message: String? = nil,
         okTitle: String? = "확인",
@@ -25,30 +40,15 @@ public enum AlertFactory {
         return alertVC
     }
     
-    public static func makeOneButtonAlert(
-        title: String? = nil,
-        message: String? = nil,
-        okTitle: String? = "확인",
-        okAction: (() -> Void)? = nil
-    ) -> UIAlertController {
-        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAlert = UIAlertAction(title: okTitle, style: .default) { _ in
-            okAction?()
-        }
-        
-        alertVC.addAction(okAlert)
-        return alertVC
-    }
-    
     public static func makeTextFieldAlert(
         title: String? = nil,
         okTitle: String? = "OK",
         placeholder: String? = nil,
-        okAction: @escaping (String?) -> Void
+        okAction: ((String?) -> Void)? = nil
     ) -> UIAlertController {
         let alertVC = UIAlertController(title: title, message: nil, preferredStyle: .alert)
         let okAlert = UIAlertAction(title: okTitle, style: .default) { _ in
-            okAction(alertVC.textFields?[0].text)
+            okAction?(alertVC.textFields?[0].text)
         }
         let cancelAlert = UIAlertAction(title: "취소", style: .cancel)
         
--- iOS/moti/moti/Presentation/Sources/Presentation/Common/BaseViewController.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/Common/BaseViewController.swift
@@ -17,4 +17,51 @@ class BaseViewController<LayoutView: UIView>: UIViewController {
         }
         view = layoutView
     }
+    
+    func showOneButtonAlert(
+        title: String? = nil,
+        message: String? = nil,
+        okTitle: String? = "확인",
+        okAction: (() -> Void)? = nil
+    ){
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAlert = UIAlertAction(title: okTitle, style: .default) { _ in
+            okAction?()
+        }
+        
+        alertVC.addAction(okAlert)
+        present(alertVC, animated: true)
+    }
+    
+    func showTwoButtonAlert(
+        title: String? = nil,
+        message: String? = nil,
+        okTitle: String? = "확인",
+        okAction: (() -> Void)? = nil
+    ){
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAlert = UIAlertAction(title: okTitle, style: .default) { _ in
+            okAction?()
+        }
+        let cancelAlert = UIAlertAction(title: "취소", style: .cancel)
+        
+        alertVC.addAction(cancelAlert)
+        alertVC.addAction(okAlert)
+        present(alertVC, animated: true)
+    }
+    
+    func showTextFieldAlert(
+        title: String? = nil,
+        okTitle: String? = "OK",
+        placeholder: String? = nil,
+        okAction: ((String?) -> Void)? = nil
+    ){
+        let alertVC = AlertFactory.makeTextFieldAlert(
+            title: "추가할 카테고리 이름을 입력하세요.",
+            okTitle: "생성",
+            placeholder: "카테고리 이름은 최대 10글자입니다.",
+            okAction: okAction
+        )
+        present(alertVC, animated: true)
+    }
 }
