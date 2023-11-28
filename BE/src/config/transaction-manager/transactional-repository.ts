00b2ce16ae146bb74ea3e3ff8ecee9a368d0b0e0
Dltmd202 <dltmd202@gmail.com>
2023-11-29 00:18:37 +0900

    [BE] feat: TransactionalRepository 업데이트
    - save 오버라이딩

--- BE/src/config/transaction-manager/transactional-repository.ts
+++ BE/src/config/transaction-manager/transactional-repository.ts
@@ -1,4 +1,4 @@
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository, SaveOptions } from 'typeorm';
 import { ObjectLiteral } from 'typeorm/common/ObjectLiteral';
 import { retrieveQueryRunner } from './index';
 
@@ -7,5 +7,16 @@ export class TransactionalRepository<
 > extends Repository<Entity> {
   get repository(): Repository<Entity> {
     return retrieveQueryRunner()?.manager.getRepository(this.target) || this;
+  }
+
+  save<T extends DeepPartial<Entity>>(
+    entity: T,
+    options?: SaveOptions,
+  ): Promise<T & Entity> {
+    Object.keys(entity).forEach((key) => {
+      entity[key] = entity[key] ?? undefined;
+    });
+
+    return super.save(entity, options);
   }
 }
