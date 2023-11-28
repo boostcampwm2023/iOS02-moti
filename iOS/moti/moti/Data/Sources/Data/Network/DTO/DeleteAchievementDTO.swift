9058b3582b981cd7a1c4d458cda09d9500d896f0
looloolalaa <dlrlgus65@nate.com>
2023-11-28 21:37:18 +0900

    [iOS] feat: 달성기록 삭제 DTO 추가

---
+++ iOS/moti/moti/Data/Sources/Data/Network/DTO/DeleteAchievementDTO.swift
@@ -0,0 +1,19 @@
+//
+//  DeleteAchievementDTO.swift
+//
+//
+//  Created by Kihyun Lee on 11/28/23.
+//
+
+import Foundation
+import Domain
+
+struct DeleteAchievementResponseDTO: ResponseDTO {
+    let success: Bool?
+    let message: String?
+    let data: DeleteAchievementDataDTO?
+}
+
+struct DeleteAchievementDataDTO: Codable {
+    let id: Int?
+}
