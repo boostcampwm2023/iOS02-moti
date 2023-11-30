a1d8306d7a7e049fc5ee8f9f1db58a347d42ce33
lsh23 <shseoul14@gmail.com>
2023-11-30 16:24:56 +0900

    [BE] feat: swagger 프로퍼티를 추가한다.

--- BE/src/group/group/dto/group-list-response.ts
+++ BE/src/group/group/dto/group-list-response.ts
@@ -1,6 +1,8 @@
 import { GroupPreview } from './group-preview.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GroupListResponse {
+  @ApiProperty({ description: '그룹 목록', type: [GroupPreview] })
   data: GroupPreview[];
 
   constructor(groupPreviews: GroupPreview[]) {
--- BE/src/group/group/dto/group-preview.dto.ts
+++ BE/src/group/group/dto/group-preview.dto.ts
@@ -1,11 +1,17 @@
 import { IGroupPreview } from '../index';
 import { dateFormat } from '../../../common/utils/date-formatter';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GroupPreview {
+  @ApiProperty({ description: '그룹 아이디' })
   id: number;
+  @ApiProperty({ description: '그룹 이름' })
   name: string;
+  @ApiProperty({ description: '그룹 로고 Url' })
   avatarUrl: string;
+  @ApiProperty({ description: '그룹 달성기록 총 회수' })
   continued: number;
+  @ApiProperty({ description: '그룹 달성기록 최근 등록 일자' })
   lastChallenged: string;
 
   constructor(groupPreview: IGroupPreview) {
