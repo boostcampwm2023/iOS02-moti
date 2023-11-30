ff948d0da8bd7522dc2e69ea2cc666d3a88e881c
Dltmd202 <dltmd202@gmail.com>
2023-11-30 16:11:59 +0900

    [BE] feat: group-fixture 업데이트
    - 영속된 group 반환

--- BE/test/group/group/group-test.module.ts
+++ BE/test/group/group/group-test.module.ts
@@ -1,11 +1,15 @@
-import { GroupFixture } from './group-fixture';
-import { GroupRepository } from '../../../src/group/group/entities/group.repository';
-import { CustomTypeOrmModule } from '../../../src/config/typeorm/custom-typeorm.module';
 import { Module } from '@nestjs/common';
+import { GroupFixture } from './group-fixture';
+import { CustomTypeOrmModule } from '../../../src/config/typeorm/custom-typeorm.module';
+import { GroupRepository } from '../../../src/group/group/entities/group.repository';
+import { GroupModule } from '../../../src/group/group/group.module';
 
 @Module({
-  imports: [CustomTypeOrmModule.forCustomRepository([GroupRepository])],
-  providers: [GroupFixture],
+  imports: [
+    CustomTypeOrmModule.forCustomRepository([GroupRepository]),
+    GroupModule,
+  ],
   exports: [GroupFixture],
+  providers: [GroupFixture],
 })
 export class GroupTestModule {}
