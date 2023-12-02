06d5e9a8705b73ffac5864604890c0a718f48edd
유정주 <dbwjdwn9216@gmail.com>
2023-12-02 17:35:30 +0900

    [iOS] feat: AppInfoCoordinator 구현

--- iOS/moti/moti/Presentation/Sources/Presentation/AppInfo/AppInfoCoordinator.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/AppInfo/AppInfoCoordinator.swift
@@ -1,8 +1,35 @@
 //
-//  File.swift
-//  
+//  AppInfoCoordinator.swift
+//
 //
 //  Created by 유정주 on 12/2/23.
 //
 
-import Foundation
+import UIKit
+import Core
+import Domain
+
+final class AppInfoCoordinator: Coordinator {
+    var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    
+    init(
+        _ navigationController: UINavigationController,
+        _ parentCoordinator: Coordinator?
+    ) {
+        self.navigationController = navigationController
+        self.parentCoordinator = parentCoordinator
+    }
+    
+    func start() {
+        guard let latest = UserDefaults.standard.readString(key: .latestVersion),
+              let required = UserDefaults.standard.readString(key: .requiredVersion),
+              let privacyPolicy = UserDefaults.standard.readString(key: .privacyPolicy) else { return }
+        
+        let version = Version(latest: latest, required: required, privacyPolicy: privacyPolicy)
+        let appInfoVC = AppInfoViewController(version: version)
+        appInfoVC.coordinator = self
+        navigationController.pushViewController(appInfoVC, animated: true)
+    }
+}
--- iOS/moti/moti/Presentation/Sources/Presentation/AppInfo/AppInfoViewController.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/AppInfo/AppInfoViewController.swift
@@ -6,9 +6,25 @@
 //
 
 import UIKit
+import Domain
 
 final class AppInfoViewController: BaseViewController<AppInfoView> {
 
+    // MARK: - Properties
+    weak var coordinator: AppInfoCoordinator?
+    private let version: Version
+    
+    // MARK: - Init
+    init(version: Version) {
+        self.version = version
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
