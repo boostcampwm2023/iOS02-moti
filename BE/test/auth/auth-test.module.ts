747a48070c9a2446c0daa0cb67b00d3f6f1dae24
lsh23 <shseoul14@gmail.com>
2023-12-02 16:27:21 +0900

    [BE] fix: 잘못된 provider 삭제

--- BE/test/auth/auth-test.module.ts
+++ BE/test/auth/auth-test.module.ts
@@ -4,11 +4,10 @@ import { AuthModule } from '../../src/auth/auth.module';
 import { AuthFixture } from './auth-fixture';
 import { AdminTestModule } from '../admin/admin-test.module';
 import { StubOauthHandler } from './StubOauthHandler';
-import { E2EAuthFixture } from './auth-e2e-fixture';
 
 @Module({
   imports: [UsersTestModule, AuthModule, AdminTestModule],
   exports: [AuthFixture],
-  providers: [AuthFixture, StubOauthHandler, E2EAuthFixture],
+  providers: [AuthFixture, StubOauthHandler],
 })
 export class AuthTestModule {}
