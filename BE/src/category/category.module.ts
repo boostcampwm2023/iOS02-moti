6b96523cc25213b62b69cbe1f313f0b43f7d78ad
Dltmd202 <dltmd202@gmail.com>
2023-12-03 21:08:59 +0900

    [BE] fix: 카테고리 리스트 조회 수정

--- BE/src/category/category.module.ts
+++ BE/src/category/category.module.ts
@@ -4,10 +4,16 @@ import { CategoryService } from './application/category.service';
 import { CustomTypeOrmModule } from '../config/typeorm/custom-typeorm.module';
 import { CategoryRepository } from './entities/category.repository';
 import { CategoryLegacyController } from './controller/category-legacy.controller';
+import { AchievementRepository } from '../achievement/entities/achievement.repository';
 
 @Module({
   controllers: [CategoryController, CategoryLegacyController],
-  imports: [CustomTypeOrmModule.forCustomRepository([CategoryRepository])],
+  imports: [
+    CustomTypeOrmModule.forCustomRepository([
+      CategoryRepository,
+      AchievementRepository,
+    ]),
+  ],
   providers: [CategoryService],
 })
 export class CategoryModule {}
