df0ad060e4b725e9df9101420ed1daf6205266f9
유정주 <dbwjdwn9216@gmail.com>
2023-12-03 12:38:10 +0900

    [iOS] feat: 버전, 자동로그인 진행 상황 표시

--- iOS/moti/moti/Presentation/Sources/Presentation/Launch/LaunchView.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/Launch/LaunchView.swift
@@ -10,12 +10,25 @@ import Design
 
 final class LaunchView: UIView {
     
+    // MARK: - Views
     private let logoImageView = {
         let imageView = UIImageView(image: MotiImage.logoWhite)
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
     
+    let progressLabel = {
+        let label = UILabel()
+        label.font = .medium
+        label.textColor = .white
+        return label
+    }()
+    
+    private var timer: Timer?
+    private var progressMessage: String = ""
+    private var dotCount = 0
+    
+    // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -26,9 +39,26 @@ final class LaunchView: UIView {
         setupUI()
     }
     
+    // MARK: - Methods
+    func update(progressMessage: String) {
+        timer?.invalidate()
+        
+        self.progressMessage = progressMessage
+        progressLabel.text = progressMessage
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            guard let self else { return }
+            dotCount += 1
+            if dotCount > 3 { dotCount = 0 }
+            let nextText = progressMessage + String(repeating: ".", count: dotCount)
+            progressLabel.text = nextText
+        }
+    }
+    
+    // MARK: - Setup
     private func setupUI() {
         backgroundColor = .primaryBlue
         setupLogoImageView()
+        setupProgressLabel()
     }
     
     private func setupLogoImageView() {
@@ -36,5 +66,12 @@ final class LaunchView: UIView {
         logoImageView.atl
             .size(width: 158, height: 65)
             .center(of: self)
+    }
+    
+    private func setupProgressLabel() {
+        addSubview(progressLabel)
+        progressLabel.atl
+            .centerX(equalTo: safeAreaLayoutGuide.centerXAnchor)
+            .bottom(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -40)
     }
 }
