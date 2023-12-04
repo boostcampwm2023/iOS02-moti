b70848ac11ec1267716322d60011a7218e8775f4
looloolalaa <dlrlgus65@nate.com>
2023-12-05 02:49:43 +0900

    [iOS] feat: 그룹원 리스트 DTO 구현

---
+++ iOS/moti/moti/Data/Sources/Data/Network/DTO/FetchGroupMemberListDTO.swift
@@ -0,0 +1,37 @@
+//
+//  FetchGroupMemberListDTO.swift
+//
+//
+//  Created by Kihyun Lee on 12/5/23.
+//
+
+import Foundation
+import Domain
+
+struct FetchGroupMemberListResponseDTO: ResponseDataDTO {
+    let success: Bool?
+    let message: String?
+    let data: FetchGroupMemberListDTO?
+}
+
+struct FetchGroupMemberListDTO: Codable {
+    let data: [GroupMemberDTO]?
+}
+
+struct GroupMemberDTO: Codable {
+    let userCode: String
+    let avatarUrl: URL?
+    let lastChallenged: Date?
+    let grade: String?
+}
+
+extension GroupMember {
+    init(dto: GroupMemberDTO) {
+        self.init(
+            userCode: dto.userCode,
+            avatarUrl: dto.avatarUrl,
+            lastChallenged: dto.lastChallenged,
+            grade: GroupGrade(grade: dto.grade ?? "") ?? .participant
+        )
+    }
+}
