ae21543cb7f519779da93a38240e3f48a8b7805c
looloolalaa <dlrlgus65@nate.com>
2023-12-04 20:42:00 +0900

    [iOS] feat: TableViewCell identifier 추가

---
+++ iOS/moti/moti/Presentation/Sources/Presentation/Extension/UITableView+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UITableView+Extension.swift
+//
+//
+//  Created by Kihyun Lee on 12/4/23.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
