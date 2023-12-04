c6d014308f9303ff6f6e181bc6094585d5ee427f
looloolalaa <dlrlgus65@nate.com>
2023-12-04 18:02:51 +0900

    [iOS] feat: GroupInfo코디네이터 구현

---
+++ iOS/moti/moti/Presentation/Sources/Presentation/GroupInfo/GroupInfoCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  GroupInfoCoordinator.swift
+//
+//
+//  Created by Kihyun Lee on 12/4/23.
+//
+
+import UIKit
+import Core
+import Domain
+
+final class GroupInfoCoordinator: Coordinator {
+    var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    
+    init(
+        _ navigationController: UINavigationController,
+        _ parentCoordinator: Coordinator?
+    ) {
+        self.navigationController = navigationController
+        self.parentCoordinator = parentCoordinator
+    }
+    
+    func start() { }
+    
+    func start(group: Group) {
+        let groupInfoVC = GroupInfoViewController(group: group)
+        groupInfoVC.coordinator = self
+        navigationController.pushViewController(groupInfoVC, animated: true)
+    }
+    
+}
