7b3f6bcd031bfca7bdc71faf34f13bb0573a98f5
looloolalaa <dlrlgus65@nate.com>
2023-12-05 02:52:35 +0900

    [iOS] feat: 그룹원 리스트 뷰 base 구현

---
+++ iOS/moti/moti/Presentation/Sources/Presentation/GroupMember/GroupMemberView.swift
@@ -0,0 +1,31 @@
+//
+//  GroupMemberView.swift
+//
+//
+//  Created by Kihyun Lee on 12/5/23.
+//
+
+import UIKit
+
+final class GroupMemberView: UIView {
+    
+    // MARK: - Views
+    // tableView
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+}
+
+extension GroupMemberView {
+    private func setupUI() {
+        
+    }
+}
