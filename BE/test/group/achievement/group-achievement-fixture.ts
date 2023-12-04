2cea1af135c53bbccdfcd6e41cbd8227f0a106bc
Dltmd202 <dltmd202@gmail.com>
2023-12-04 16:55:53 +0900

    [BE] feat: GroupAchievementFixture 업데이트
    - 다수의 그룹 달성기록 픽스처

--- BE/test/group/achievement/group-achievement-fixture.ts
+++ BE/test/group/achievement/group-achievement-fixture.ts
@@ -4,6 +4,8 @@ import { Group } from '../../../src/group/group/domain/group.domain';
 import { User } from '../../../src/users/domain/user.domain';
 import { Injectable } from '@nestjs/common';
 import { GroupAchievementRepository } from '../../../src/group/achievement/entities/group-achievement.repository';
+import { Image } from '../../../src/image/domain/image.domain';
+import { ImageFixture } from '../../image/image-fixture';
 
 @Injectable()
 export class GroupAchievementFixture {
@@ -11,22 +13,45 @@ export class GroupAchievementFixture {
 
   constructor(
     private readonly groupAchievementRepository: GroupAchievementRepository,
+    private readonly imageFixture: ImageFixture,
   ) {}
   async createGroupAchievement(
     user: User,
     group: Group,
     groupCategory: GroupCategory,
     name?: string,
+    image?: Image,
   ) {
+    image = image || (await this.imageFixture.getImage(user));
     const groupAchievement = GroupAchievementFixture.groupAchievement(
       user,
       group,
       groupCategory,
       name,
+      image,
     );
     return await this.groupAchievementRepository.saveGroupAchievement(
       groupAchievement,
     );
+  }
+
+  async createGroupAchievements(
+    count: number,
+    user: User,
+    group: Group,
+    groupCategory: GroupCategory,
+  ) {
+    const groupAchievements: GroupAchievement[] = [];
+    for (let i = 0; i < count; i++) {
+      const groupAchievement = await this.createGroupAchievement(
+        user,
+        group,
+        groupCategory,
+      );
+      groupAchievements.push(groupAchievement);
+    }
+
+    return groupAchievements;
   }
 
   static groupAchievement(
@@ -34,6 +59,7 @@ export class GroupAchievementFixture {
     group: Group,
     groupCategory: GroupCategory,
     name?: string,
+    image?: Image,
   ): GroupAchievement {
     return new GroupAchievement(
       name || `제목${++GroupAchievementFixture.id}`,
@@ -41,6 +67,7 @@ export class GroupAchievementFixture {
       group,
       groupCategory,
       `내용${GroupAchievementFixture.id}`,
+      image || ImageFixture.image(user),
     );
   }
 }
