dd526ca5751288aebf32e3f033171906078d1c15
looloolalaa <dlrlgus65@nate.com>
2023-12-05 03:50:29 +0900

    [iOS] feat: 그룹원 리스트 뷰 - 컬렉션 뷰 구현

--- iOS/moti/moti/Presentation/Sources/Presentation/GroupMember/GroupMemberView.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/GroupMember/GroupMemberView.swift
@@ -10,7 +10,12 @@ import UIKit
 final class GroupMemberView: UIView {
     
     // MARK: - Views
-    // tableView
+    private(set) lazy var groupMemberCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
+        collectionView.backgroundColor = .motiBackground
+        collectionView.register(with: GroupMemberCollectionViewCell.self)
+        return collectionView
+    }()
     
     // MARK: - Init
     override init(frame: CGRect) {
@@ -22,10 +27,32 @@ final class GroupMemberView: UIView {
         super.init(coder: coder)
         setupUI()
     }
+    
+    private func setupUI() {
+        addSubview(groupMemberCollectionView)
+        groupMemberCollectionView.atl
+            .top(equalTo: safeAreaLayoutGuide.topAnchor)
+            .bottom(equalTo: bottomAnchor)
+            .horizontal(equalTo: safeAreaLayoutGuide)
+    }
 }
 
-extension GroupMemberView {
-    private func setupUI() {
+private extension GroupMemberView {
+    func makeCollectionViewLayout() -> UICollectionViewLayout {
+        let itemPadding: CGFloat = 20
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(80))
+        let itemInset = NSDirectionalEdgeInsets(top: 0, leading: itemPadding, bottom: 0, trailing: itemPadding)
+        let groupInset = NSDirectionalEdgeInsets(top: itemPadding, leading: 0, bottom: itemPadding, trailing: 0)
+        let groupEdgeSpacing = NSCollectionLayoutEdgeSpacing(leading: .none, top: .fixed(5), trailing: .none, bottom: .fixed(5))
         
+        return CompositionalLayoutFactory.makeVerticalCompositionalLayout(
+            itemSize: itemSize,
+            itemInset: itemInset,
+            groupSize: itemSize,
+            groupInset: groupInset,
+            groupEdgeSpacing: groupEdgeSpacing
+        )
     }
 }
