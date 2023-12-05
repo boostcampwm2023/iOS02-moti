a3c292c252a3a4170e2bc729e58e738a354eace8
looloolalaa <dlrlgus65@nate.com>
2023-12-05 18:27:54 +0900

    [iOS] feat: 그룹원 리스트 contextMenu 구현

--- iOS/moti/moti/Presentation/Sources/Presentation/GroupMember/Cell/GroupMemberCollectionViewCell.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/GroupMember/Cell/GroupMemberCollectionViewCell.swift
@@ -58,8 +58,8 @@ final class GroupMemberCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
-    private let gradeButton = {
-        let button = UIButton()
+    private var gradeButton = {
+        let button = UIButton(type: .system)
         button.setTitleColor(.label, for: .normal)
         return button
     }()
@@ -76,13 +76,45 @@ final class GroupMemberCollectionViewCell: UICollectionViewCell {
     }
     
     // MARK: - Methods
-    func configure(with groupMember: GroupMember) {
+    func configureForMember(with groupMember: GroupMember) {
         if let url = groupMember.user.avatarURL {
             iconImageView.jf.setImage(with: url)
         }
         userCodeLabel.text = "@" + groupMember.user.code
         lastChallengedLabel.text = groupMember.lastChallenged?.convertStringyyyy_MM_dd() ?? "없음"
-        gradeButton.setTitle(groupMember.grade.description, for: .normal)
+        gradeButton.setTitle(groupMember.grade.description + " ", for: .normal)
+    }
+    
+    func configureForLeader(with groupMember: GroupMember) {
+        configureForMember(with: groupMember)
+        if groupMember.grade != .leader {
+            setupGradeButtonForLeader()
+        }
+    }
+    
+    private func setupGradeButtonForLeader() {
+        gradeButton.configuration = .plain()
+        gradeButton.setImage(SymbolImage.chevronUpDown, for: .normal)
+        gradeButton.tintColor = .label
+        gradeButton.configuration?.imagePlacement = .trailing
+        gradeButton.configuration?.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: -5)
+        
+        gradeButton.showsMenuAsPrimaryAction = true
+        let managerAction = UIAction(title: "관리자") { _ in
+            self.managerMenuDidClicked()
+        }
+        let memberAction = UIAction(title: "그룹원") { _ in
+            self.memberMenuDidClicked()
+        }
+        gradeButton.menu = UIMenu(children: [managerAction, memberAction])
+    }
+    
+    private func managerMenuDidClicked() {
+        gradeButton.setTitle("관리자", for: .normal)
+    }
+    
+    private func memberMenuDidClicked() {
+        gradeButton.setTitle("그룹원", for: .normal)
     }
     
     func cancelDownloadImage() {
