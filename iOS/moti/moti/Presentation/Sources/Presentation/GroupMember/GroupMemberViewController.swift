7fc06bf2495f09312a4ac8253f5744c3b54e3040
looloolalaa <dlrlgus65@nate.com>
2023-12-06 01:10:10 +0900

    [iOS] feat: 권한수정 action 호출

--- iOS/moti/moti/Presentation/Sources/Presentation/GroupMember/GroupMemberViewController.swift
+++ iOS/moti/moti/Presentation/Sources/Presentation/GroupMember/GroupMemberViewController.swift
@@ -93,9 +93,9 @@ extension GroupMemberViewController: UICollectionViewDelegate {
 
 extension GroupMemberViewController: GroupMemberCollectionViewCellDelegate {
     func menuDidClicked(groupMember: GroupMember, newGroupGrade: GroupGrade) {
-        showTwoButtonAlert(title: "매니저 권한으로 수정하시겠습니까?", okTitle: "수정") { [weak self] in
+        showTwoButtonAlert(title: "\(newGroupGrade.description) 권한으로 수정하시겠습니까?", okTitle: "수정") { [weak self] in
             guard let self else { return }
-            print("뷰모델 함수 호출 예정", self.viewModel.findIndex(groupMember: groupMember))
+            self.viewModel.action(.updateGrade(groupMember: groupMember, newGroupGrade: newGroupGrade))
         }
     }
 }
