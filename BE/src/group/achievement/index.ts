8d7e65ff8eebe6ad2e4c461f726291bb251100f3
lsh23 <shseoul14@gmail.com>
2023-12-06 01:36:27 +0900

    [BE] feat: 그룹 달성 기록 리스트 API에 필요한 type을 추가한다.

--- BE/src/group/achievement/index.ts
+++ BE/src/group/achievement/index.ts
@@ -3,3 +3,11 @@ import { IAchievementDetail } from '../../achievement';
 export interface IGroupAchievementDetail extends IAchievementDetail {
   userCode: string;
 }
+
+export interface IGroupAchievementListDetail {
+  id: number;
+  title: string;
+  thumbnailUrl: string;
+  userCode: string;
+  categoryId: number;
+}
