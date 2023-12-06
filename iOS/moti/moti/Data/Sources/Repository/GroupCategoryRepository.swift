a531cecfc0cbb3a0f0eca5794c46309437d71865
유정주 <dbwjdwn9216@gmail.com>
2023-12-06 13:01:05 +0900

    [iOS] feat: 샘플 JSON 제거

--- iOS/moti/moti/Data/Sources/Repository/GroupCategoryRepository.swift
+++ iOS/moti/moti/Data/Sources/Repository/GroupCategoryRepository.swift
@@ -17,40 +17,10 @@ public struct GroupCategoryRepository: CategoryListRepositoryProtocol {
         self.groupId = groupId
     }
     
-    public func fetchCategoryList() async throws -> [CategoryItem] {
-        let sampleJson = """
-        {
-            "success": true,
-            "data": [
-                {
-                    "id": 0,
-                    "name": "전체",
-                    "continued": 150,
-                    "lastChallenged": "2011-04-10T20:09:31Z"
-                },
-                {
-                    "id": -1,
-                    "name": "미설정",
-                    "continued": 150,
-                    "lastChallenged": "2011-04-10T20:09:31Z"
-                },
-                {
-                    "id": 1000,
-                    "name": "다이어트",
-                    "continued": 10,
-                    "lastChallenged": "2011-04-10T20:09:31Z"
-                }
-            ]
-        }
-        """
-        
+    public func fetchCategoryList() async throws -> [CategoryItem] {        
         // 실제 네트워크 통신
-//        let endpoint = MotiAPI.fetchGroupCategoryList(groupId: groupId)
-//        let responseDTO = try await provider.request(with: endpoint, type: CategoryListResponseDTO.self)
-        guard let testData = sampleJson.data(using: .utf8) else { throw NetworkError.decode }
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .iso8601
-        let responseDTO = try decoder.decode(CategoryListResponseDTO.self, from: testData)
+        let endpoint = MotiAPI.fetchGroupCategoryList(groupId: groupId)
+        let responseDTO = try await provider.request(with: endpoint, type: CategoryListResponseDTO.self)
         
         guard let categoryDTO = responseDTO.data else { throw NetworkError.decode }
         return categoryDTO.map { CategoryItem(dto: $0) }
