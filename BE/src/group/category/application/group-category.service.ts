036c274b7203529335e0b4c1125847fffe657709
Dltmd202 <dltmd202@gmail.com>
2023-12-06 11:47:22 +0900

    [BE] feat: GroupCategoryService 업데이트
    - 그룹 도전기록 조회함수

--- BE/src/group/category/application/group-category.service.ts
+++ BE/src/group/category/application/group-category.service.ts
@@ -6,6 +6,7 @@ import { GroupCategoryCreate } from '../dto/group-category-create';
 import { GroupRepository } from '../../group/entities/group.repository';
 import { UnauthorizedGroupCategoryException } from '../exception/unauthorized-group-category.exception';
 import { GroupCategory } from '../domain/group.category';
+import { UnauthorizedApproachGroupCategoryException } from '../exception/unauthorized-approach-group-category.exception';
 
 @Injectable()
 export class GroupCategoryService {
@@ -20,17 +21,30 @@ export class GroupCategoryService {
     groupId: number,
     groupCtgCreate: GroupCategoryCreate,
   ): Promise<GroupCategory> {
-    const group = await this.getGroup(user, groupId);
+    const group = await this.getGroupByLeader(user, groupId);
     const groupCategory = groupCtgCreate.toModel(user, group);
     return this.groupCategoryRepository.saveGroupCategory(groupCategory);
   }
 
-  private async getGroup(user: User, groupId: number) {
+  @Transactional({ readonly: true })
+  async retrieveCategoryMetadata(user: User, groupId: number) {
+    const group = await this.getGroup(user, groupId);
+    return this.groupCategoryRepository.findGroupCategoriesByUser(user, group);
+  }
+
+  private async getGroupByLeader(user: User, groupId: number) {
     const group = await this.groupRepository.findGroupByIdAndLeaderUser(
       user,
       groupId,
     );
     if (!group) throw new UnauthorizedGroupCategoryException();
+
+    return group;
+  }
+
+  private async getGroup(user: User, groupId: number) {
+    const group = await this.groupRepository.findByIdAndUser(groupId, user);
+    if (!group) throw new UnauthorizedApproachGroupCategoryException();
 
     return group;
   }
