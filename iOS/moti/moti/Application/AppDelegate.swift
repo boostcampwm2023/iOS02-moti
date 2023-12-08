c182ff33bd0c39b90b00cc8eb8a83870660ab709
유정주 <dbwjdwn9216@gmail.com>
2023-12-08 15:56:09 +0900

    [iOS] feat: 아이패드 회전 제한, 한 줄에 9개 표시

--- iOS/moti/moti/Application/AppDelegate.swift
+++ iOS/moti/moti/Application/AppDelegate.swift
@@ -29,14 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(
-        _ application: UIApplication,
-        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
-    ) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, 
-        // this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return [.portrait, .portraitUpsideDown]
     }
-
 }
