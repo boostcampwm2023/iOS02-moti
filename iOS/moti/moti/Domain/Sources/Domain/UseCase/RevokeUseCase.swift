986b9b74788b2786e40c4ea2b2b671a9b91d41cd
유정주 <dbwjdwn9216@gmail.com>
2023-12-09 23:46:38 +0900

    [iOS] feat: Revoke 도메인 정의

---
+++ iOS/moti/moti/Domain/Sources/Domain/UseCase/RevokeUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  RevokeUseCase.swift
+//
+//
+//  Created by 유정주 on 12/9/23.
+//
+
+import Foundation
+
+public struct RevokeRequestValue: RequestValue {
+    public let identityToken: String
+    public let authrizationCode: String
+    
+    public init(identityToken: String, authrizationCode: String) {
+        self.identityToken = identityToken
+        self.authrizationCode = authrizationCode
+    }
+}
+
+public struct RevokeUseCase {
+    private let repository: AuthRepositoryProtocol
+    
+    public init(repository: AuthRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    func execute(requestValue: RevokeRequestValue) async throws -> Bool {
+        return try await repository.revoke(requestValue: requestValue)
+    }
+}
