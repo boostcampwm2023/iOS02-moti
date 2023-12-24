40fb1e8c3c8a7c1b2e2584813fbfe0f599dc6299
Dltmd202 <dltmd202@gmail.com>
2023-12-24 21:57:22 +0900

    [BE] feat: CategoryRelocateRequest 생성

---
+++ BE/src/category/dto/category-relocate.request.ts
@@ -0,0 +1,16 @@
+import { IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CategoryRelocateRequest {
+  @IsArray({
+    message: '요청 형식이 잘 못 되었습니다.',
+  })
+  @ApiProperty({
+    description: '변경할 카테고리 아이디의 순서',
+  })
+  order: number[];
+
+  getCategoryCount() {
+    return this.order.length;
+  }
+}
