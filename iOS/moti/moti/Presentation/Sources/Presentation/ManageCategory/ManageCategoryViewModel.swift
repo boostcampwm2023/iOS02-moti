392049b1bf8f4f2fc138bd579adae739cd172b30
looloolalaa <dlrlgus65@nate.com>
2023-12-26 03:23:40 +0900

    [iOS] feat: 카테고리 관리 VM 구현

---
+++ iOS/moti/moti/Presentation/Sources/Presentation/ManageCategory/ManageCategoryViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ManageCategoryViewModel.swift
+//
+//
+//  Created by Kihyun Lee on 12/25/23.
+//
+
+import Foundation
+import Domain
+import Core
+import Combine
+
+final class ManageCategoryViewModel {
+    enum ManageCategoryViewModelAction {
+        
+    }
+    
+    typealias CategoryDataSource = ListDiffableDataSource<CategoryItem>
+    
+    // MARK: - Properties
+    private var categoryDataSource: CategoryDataSource?
+    var categories: [CategoryItem] = [] {
+        didSet {
+            categoryDataSource?.update(data: categories)
+        }
+    }
+    
+    init(
+        categories: [CategoryItem]
+    ) {
+        self.categories = categories
+    }
+    
+    func setupDataSource(_ dataSource: CategoryDataSource) {
+        self.categoryDataSource = dataSource
+        categoryDataSource?.update(data: categories)
+    }
+}
