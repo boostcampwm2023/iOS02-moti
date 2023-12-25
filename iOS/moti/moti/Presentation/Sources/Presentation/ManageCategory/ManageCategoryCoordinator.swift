b818b757c499f33664a6406a27e4ed5cd3da1f24
looloolalaa <dlrlgus65@nate.com>
2023-12-26 03:24:04 +0900

    [iOS] feat: 카테고리 관리 코디네이터 구현

---
+++ iOS/moti/moti/Presentation/Sources/Presentation/ManageCategory/ManageCategoryCoordinator.swift
@@ -0,0 +1,49 @@
+//
+//  ManageCategoryCoordinator.swift
+//
+//
+//  Created by Kihyun Lee on 12/25/23.
+//
+
+import UIKit
+import Core
+import Domain
+import Data
+
+final class ManageCategoryCoordinator: Coordinator {
+    var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    
+    init(
+        _ navigationController: UINavigationController,
+        _ parentCoordinator: Coordinator?
+    ) {
+        self.navigationController = navigationController
+        self.parentCoordinator = parentCoordinator
+    }
+    
+    func start() { }
+    
+    func start(categories: [CategoryItem]) {
+        let manageCategoryVM = ManageCategoryViewModel(
+            categories: categories
+        )
+        let manageCategoryVC = ManageCategoryViewController(viewModel: manageCategoryVM)
+        manageCategoryVC.coordinator = self
+        manageCategoryVC.delegate = self
+        let navVC = UINavigationController(rootViewController: manageCategoryVC)
+        navVC.modalPresentationStyle = .overFullScreen
+        navigationController.present(navVC, animated: true)
+    }
+}
+
+extension ManageCategoryCoordinator: ManageCategoryViewControllerDelegate {
+    func cancelButtonDidClicked() {
+        parentCoordinator?.dismiss(child: self, animated: true)
+    }
+    
+    func doneButtonDidClicked() {
+        parentCoordinator?.dismiss(child: self, animated: true)
+    }
+}
