9608bc91e6814e6d7cd71a1a2efe941ea5589e0c
Dltmd202 <dltmd202@gmail.com>
2023-12-27 13:27:10 +0900

    [BE] feat: 카테고리 삭제 API 테스트

--- BE/src/category/application/category.service.spec.ts
+++ BE/src/category/application/category.service.spec.ts
@@ -18,6 +18,9 @@ import { transactionTest } from '../../../test/common/transaction-test';
 import { NotFoundCategoryException } from '../exception/not-found-category.exception';
 import { CategoryRelocateRequest } from '../dto/category-relocate.request';
 import { InvalidCategoryRelocateException } from '../exception/Invalid-Category-Relocate.exception';
+import { CategoryMetaData } from '../dto/category-metadata';
+import { CategoryNotFoundException } from '../exception/category-not-found.exception';
+import { UserRepository } from '../../users/entities/user.repository';
 
 describe('CategoryService', () => {
   let categoryService: CategoryService;
@@ -25,6 +28,7 @@ describe('CategoryService', () => {
   let usersFixture: UsersFixture;
   let categoryFixture: CategoryFixture;
   let achievementFixture: AchievementFixture;
+  let userRepository: UserRepository;
 
   beforeAll(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -40,6 +44,7 @@ describe('CategoryService', () => {
       providers: [],
     }).compile();
 
+    userRepository = app.get<UserRepository>(UserRepository);
     categoryFixture = app.get<CategoryFixture>(CategoryFixture);
     achievementFixture = app.get<AchievementFixture>(AchievementFixture);
     usersFixture = app.get<UsersFixture>(UsersFixture);
@@ -107,52 +112,56 @@ describe('CategoryService', () => {
 
   describe('getCategoriesByUsers는 카테고리를 조회할 수 있다', () => {
     it('user에 대한 카테고리가 없을 때 빈 배열을 반환한다.', async () => {
-      // given
-      const user = await usersFixture.getUser(1);
+      await transactionTest(dataSource, async () => {
+        // given
+        const user = await usersFixture.getUser(1);
 
-      // when
-      const retrievedCategories =
-        await categoryService.getCategoriesByUser(user);
+        // when
+        const retrievedCategories =
+          await categoryService.getCategoriesByUser(user);
 
-      // then
-      expect(retrievedCategories).toBeDefined();
-      expect(retrievedCategories.length).toBe(1);
-      expect(retrievedCategories[0].categoryId).toEqual(-1);
+        // then
+        expect(retrievedCategories).toBeDefined();
+        expect(retrievedCategories.length).toBe(1);
+        expect(retrievedCategories[0].categoryId).toEqual(-1);
+      });
     });
 
     it('user에 대한 카테고리가 있을 때 카테고리를 반환한다.', async () => {
-      // given
-      const user = await usersFixture.getUser(1);
-      const categories: Category[] = await categoryFixture.getCategories(
-        4,
-        user,
-      );
-      await achievementFixture.getAchievements(4, user, categories[0]);
-      await achievementFixture.getAchievements(5, user, categories[1]);
-      await achievementFixture.getAchievements(7, user, categories[2]);
-      await achievementFixture.getAchievements(10, user, categories[3]);
+      await transactionTest(dataSource, async () => {
+        // given
+        const user = await usersFixture.getUser(1);
+        const categories: Category[] = await categoryFixture.getCategories(
+          4,
+          user,
+        );
+        await achievementFixture.getAchievements(4, user, categories[0]);
+        await achievementFixture.getAchievements(5, user, categories[1]);
+        await achievementFixture.getAchievements(7, user, categories[2]);
+        await achievementFixture.getAchievements(10, user, categories[3]);
 
-      // when
-      const retrievedCategories =
-        await categoryService.getCategoriesByUser(user);
+        // when
+        const retrievedCategories =
+          await categoryService.getCategoriesByUser(user);
 
-      // then
-      expect(retrievedCategories.length).toBe(5);
-      expect(retrievedCategories[0].categoryId).toEqual(-1);
-      expect(retrievedCategories[0].insertedAt).toBeNull();
-      expect(retrievedCategories[0].achievementCount).toBe(0);
-      expect(retrievedCategories[1].categoryId).toEqual(categories[0].id);
-      expect(retrievedCategories[1].insertedAt).toBeInstanceOf(Date);
-      expect(retrievedCategories[1].achievementCount).toBe(4);
-      expect(retrievedCategories[2].categoryId).toEqual(categories[1].id);
-      expect(retrievedCategories[2].insertedAt).toBeInstanceOf(Date);
-      expect(retrievedCategories[2].achievementCount).toBe(5);
-      expect(retrievedCategories[3].categoryId).toEqual(categories[2].id);
-      expect(retrievedCategories[3].insertedAt).toBeInstanceOf(Date);
-      expect(retrievedCategories[3].achievementCount).toBe(7);
-      expect(retrievedCategories[4].categoryId).toEqual(categories[3].id);
-      expect(retrievedCategories[4].insertedAt).toBeInstanceOf(Date);
-      expect(retrievedCategories[4].achievementCount).toBe(10);
+        // then
+        expect(retrievedCategories.length).toBe(5);
+        expect(retrievedCategories[0].categoryId).toEqual(-1);
+        expect(retrievedCategories[0].insertedAt).toBeNull();
+        expect(retrievedCategories[0].achievementCount).toBe(0);
+        expect(retrievedCategories[1].categoryId).toEqual(categories[0].id);
+        expect(retrievedCategories[1].insertedAt).toBeInstanceOf(Date);
+        expect(retrievedCategories[1].achievementCount).toBe(4);
+        expect(retrievedCategories[2].categoryId).toEqual(categories[1].id);
+        expect(retrievedCategories[2].insertedAt).toBeInstanceOf(Date);
+        expect(retrievedCategories[2].achievementCount).toBe(5);
+        expect(retrievedCategories[3].categoryId).toEqual(categories[2].id);
+        expect(retrievedCategories[3].insertedAt).toBeInstanceOf(Date);
+        expect(retrievedCategories[3].achievementCount).toBe(7);
+        expect(retrievedCategories[4].categoryId).toEqual(categories[3].id);
+        expect(retrievedCategories[4].insertedAt).toBeInstanceOf(Date);
+        expect(retrievedCategories[4].achievementCount).toBe(10);
+      });
     });
   });
 
@@ -370,6 +379,86 @@ describe('CategoryService', () => {
         await expect(
           categoryService.relocateCategory(user, categoryRelocateRequest),
         ).rejects.toThrow(InvalidCategoryRelocateException);
+      });
+    });
+  });
+
+  describe('deleteCategory는 카테고리를 삭제할 수 있다.', () => {
+    it('본인의 카테고리를 삭제할 수 있다.', async () => {
+      await transactionTest(dataSource, async () => {
+        // given
+        const user = await usersFixture.getUser('ABCD');
+        const category = await categoryFixture.getCategory(user, '카테고리1');
+
+        // when
+        await categoryService.deleteCategory(user, category.id);
+        const categoryMetaData: CategoryMetaData[] =
+          await categoryService.getCategoriesByUser(user);
+
+        // then
+        expect(categoryMetaData.length).toBe(1);
+        expect(categoryMetaData[0].categoryId).toBe(-1);
+      });
+    });
+
+    it('본인의 카테고리가 아닌 카테고리는 삭제할 수 없다.', async () => {
+      await transactionTest(dataSource, async () => {
+        // given
+        const otherUser = await usersFixture.getUser('ABC');
+        const category = await categoryFixture.getCategory(
+          otherUser,
+          '카테고리1',
+        );
+
+        const user = await usersFixture.getUser('ABCD');
+
+        // when
+        await expect(
+          categoryService.deleteCategory(user, category.id),
+        ).rejects.toThrow(new CategoryNotFoundException());
+      });
+    });
+
+    it('카테고리를 삭제해도 기존 순서가 유지된다.', async () => {
+      await transactionTest(dataSource, async () => {
+        // given
+        const user = await usersFixture.getUser('ABCD');
+        const category1 = await categoryFixture.getCategory(user, '카테고리1');
+        const category2 = await categoryFixture.getCategory(user, '카테고리2');
+        const category3 = await categoryFixture.getCategory(user, '카테고리3');
+
+        // when
+        await categoryService.deleteCategory(user, category2.id);
+        const categoryMetaData: CategoryMetaData[] =
+          await categoryService.getCategoriesByUser(user);
+
+        // then
+        expect(categoryMetaData.length).toBe(3);
+        expect(categoryMetaData[0].categoryId).toBe(-1);
+        expect(categoryMetaData[1].categoryId).toBe(category1.id);
+        expect(categoryMetaData[2].categoryId).toBe(category3.id);
+      });
+    });
+
+    it('카테고리를 삭제하면 유저의 카테고리 카운트가 반영된다.', async () => {
+      await transactionTest(dataSource, async () => {
+        // given
+        const user = await usersFixture.getUser('ABCD');
+        await categoryFixture.getCategory(user, '카테고리1');
+        const category2 = await categoryFixture.getCategory(user, '카테고리2');
+        await categoryFixture.getCategory(user, '카테고리3');
+
+        // when
+        await categoryService.deleteCategory(user, category2.id);
+        const userEntity = await userRepository.repository.findOne({
+          where: {
+            id: user.id,
+          },
+        });
+
+        // then
+        expect(userEntity.categoryCount).toBe(2);
+        expect(userEntity.categorySequence).toBe(3);
       });
     });
   });
